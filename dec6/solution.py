86539e08b74ceeed2a24fae38b76ec22e2482ebd
Andreas Jessen <dressingoak@gmail.com>
2020-12-13 14:42:08 +0100

    Fixed solution for Dec 6, part 2 (the input data was wrongly read)

--- dec6/solution.py
+++ dec6/solution.py
@@ -1,7 +1,6 @@
-import re
 from functools import reduce
 
-input_data = list(map(lambda x: re.split(r'\s+|\n+', x), "".join(open("input.txt").readlines()).split('\n\n')))
+input_data = list(map(lambda x: x.split(), "".join(open("input.txt").readlines()).split('\n\n')))
 
 class Answer:
 
@@ -32,24 +31,6 @@ class Group:
         return Answer("".join(reduce(lambda x, y: x.intersection(y), [_.answer for _ in self.answers])))
 
 groups = [Group(list(Answer(_) for _ in answers)) for answers in input_data]
-# groups = [
-#     Group([Answer("abc")]),
-#     Group([Answer("a"), Answer("b"), Answer("c")]),
-#     Group([Answer("ab"), Answer("ac")]),
-#     Group([Answer("a"), Answer("a"), Answer("a"), Answer("a")]),
-#     Group([Answer("b")])
-# ]
 
 print("Part 1: {}".format(sum(_.union_answers().count() for _ in groups)))
-# print("Part 1: {}".format(list(_.union_answers().count() for _ in groups)))
 print("Part 2: {}".format(sum(_.intersect_answers().count() for _ in groups)))
-# print("Part 2: {}".format(list(_.intersect_answers().count() for _ in groups)))
-
-c = 0
-for group in groups:
-    print(group)
-    print("-> {}".format(group.intersect_answers()))
-    print()
-    c += 1
-    if c > 20:
-        break
