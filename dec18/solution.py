cfdec6ec4db714cbb56923a736725f5a09b527f0
Andreas Jessen <dressingoak@gmail.com>
2020-12-27 13:31:59 +0100

    Solution for Dec 18, part 2

--- dec18/solution.py
+++ dec18/solution.py
@@ -1,8 +1,9 @@
 import re
+from math import prod
 
 exprs = open("input.txt").read().strip().split("\n")
 
-def calculate(expr):
+def calculate_wo_precedence(expr):
     pattern = re.compile(r'([\*\+])\s*(\d+)')
     m1 = re.match(r'^(\d+)', expr)
     if m1:
@@ -20,7 +21,29 @@ def calculate(expr):
     else:
         raise Exception("Cannot evaluate expression, got '{}'".format(expr))
 
-def evaluate(expr):
+def calculate_with_precedence(expr):
+    pattern = re.compile(r'(\d+)\s*\+\s*(\d+)')
+    left = 0
+    s = ""
+    matched = False
+    for m in re.finditer(pattern, expr):
+        matched = True
+        l = m.start()
+        r = m.end()
+        first = int(m.group(1))
+        second = int(m.group(2))
+        s += expr[left:l]
+        s += str(first + second)
+        left = r
+    s += expr[left:]
+    if matched:
+        return calculate_with_precedence(s)
+    else:
+        dpattern = re.compile(r'(\d+)')
+        value = prod([int(_) for _ in re.findall(dpattern, s)])
+        return value
+
+def evaluate(expr, method):
     pattern = re.compile(r'\(([\*\+\d\s]+)\)')
     left = 0
     s = ""
@@ -30,12 +53,14 @@ def evaluate(expr):
         l = m.start()
         r = m.end()
         s += expr[left:l]
-        s += str(calculate(m.group(1)))
+        s += str(method(m.group(1)))
         left = r
     s += expr[left:]
     if matched:
-        return evaluate(s)
+        return evaluate(s, method)
     else:
-        return calculate(s)
+        return method(s)
 
-print("Part 1: {}".format(sum(evaluate(_) for _ in exprs)))
+print("Part 1: {}".format(sum(evaluate(_, calculate_wo_precedence) for _ in exprs)))
+
+print("Part 2: {}".format(sum(evaluate(_, calculate_with_precedence) for _ in exprs)))
