d18ce673910247add7972017ca4b6fb37526bf4e
Andreas Jessen <dressingoak@gmail.com>
2021-01-30 23:02:56 +0100

    Solution for Dec 1 2019, part 2 (still in Rust)

--- 2019/day1/rust/src/main.rs
+++ 2019/day1/rust/src/main.rs
@@ -1,22 +1,43 @@
 fn required_fuel(x: u32) -> u32 {
-    x / 3 - 2
+    let calc = (x / 3).checked_sub(2);
+    match calc {
+        Some(res) => res,
+        None => 0
+    }
+}
+
+fn required_fuel_including_own_weight(x: u32) -> u32 {
+    let mut total = 0u32;
+    let mut current = x;
+    loop {
+        let additional = required_fuel(current);
+        if additional == 0 {
+            break total;
+        } else {
+            total += additional;
+            current = additional;
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::required_fuel;
+    use super::*;
 
     #[test]
-    fn test_a() { assert_eq!(required_fuel(12), 2); }
+    fn test_required_fuel() { 
+        assert_eq!(required_fuel(12), 2); 
+        assert_eq!(required_fuel(14), 2);
+        assert_eq!(required_fuel(1969), 654);
+        assert_eq!(required_fuel(100756), 33583);
+    }
 
     #[test]
-    fn test_b() { assert_eq!(required_fuel(14), 2); }
-
-    #[test]
-    fn test_c() { assert_eq!(required_fuel(1969), 654); }
-
-    #[test]
-    fn test_d() { assert_eq!(required_fuel(100756), 33583); }
+    fn test_required_fuel_including_own_weight() {
+        assert_eq!(required_fuel_including_own_weight(14), 2); 
+        assert_eq!(required_fuel_including_own_weight(1969), 654 + 216 + 70 + 21 + 5);
+        assert_eq!(required_fuel_including_own_weight(100756), 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2); 
+    }
 }
 
 fn main() {
@@ -33,4 +54,15 @@ fn main() {
         .fold(0, |a, b| a + b);
 
     println!("Par1 1: {}", fuel_requirements);
+
+    let total_fuel_requirements: u32 = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file")
+        .lines()
+        .map(|x| {
+            let weight: u32 = x.parse().unwrap();
+            required_fuel_including_own_weight(weight)
+        })
+        .fold(0, |a, b| a + b);
+
+    println!("Par1 2: {}", total_fuel_requirements);
 }
