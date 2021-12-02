28488823d05b94a896c7dec77ff3ec921238b575
Andreas Jessen <dressingoak@gmail.com>
2021-12-02 09:57:04 +0100

    Solution to Dec 2, part 2

--- 2021/dec2/solution.py
+++ 2021/dec2/solution.py
@@ -7,16 +7,33 @@ except:
 
 def compute(file: str):
     horizontal = 0
-    vertical = 0
+    depth = 0
     f = open(file, "r")
     for line in f.readlines():
         match line.strip().split():
             case ["forward", x]:
                 horizontal += int(x)
             case ["down", x]:
-                vertical += int(x)
+                depth += int(x)
             case ["up", x]:
-                vertical -= int(x)
-    return horizontal * vertical
+                depth -= int(x)
+    return horizontal * depth
+
+def compute_with_aim(file: str):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    f = open(file, "r")
+    for line in f.readlines():
+        match line.strip().split():
+            case ["forward", x]:
+                horizontal += int(x)
+                depth += aim * int(x)
+            case ["down", x]:
+                aim += int(x)
+            case ["up", x]:
+                aim -= int(x)
+    return horizontal * depth
 
 print("Dec 2, part 1: {}".format(compute(file)))
+print("Dec 2, part 2: {}".format(compute_with_aim(file)))
