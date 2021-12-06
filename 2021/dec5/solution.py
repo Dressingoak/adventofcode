40ea3a3240d0fa705ec5b4a6ab1c6a3cf9566cc6
Andreas Jessen <dressingoak@gmail.com>
2021-12-06 17:37:02 +0100

    Solution to Dec 5, part 1 (less complicated) and part 2

--- 2021/dec5/solution.py
+++ 2021/dec5/solution.py
@@ -24,47 +24,46 @@ class LineSegment:
     def __init__(self, p1: Point, p2: Point):
         self.p1 = p1
         self.p2 = p2
-
-    def box(self):
-        if self.p1.x == self.p2.x:
-            lower = min(self.p1.y, self.p2.y)
-            upper = max(self.p1.y, self.p2.y)
-            return (0, self.p1.x, lower, upper)
-        elif self.p1.y == self.p2.y:
-            left = min(self.p1.x, self.p2.x)
-            right = max(self.p1.x, self.p2.x)
-            return (1, self.p1.y, left, right)
+        if p1.x == p2.x:
+            lower = min(p1.y, p2.y)
+            upper = max(p1.y, p2.y)
+            self.dir = 3 # South
+            self.p = Point(p1.x, lower)
+            self.size = upper - lower + 1
+        elif p1.y == p2.y:
+            left = min(p1.x, p2.x)
+            right = max(p1.x, p2.x)
+            self.dir = 1 # East
+            self.p = Point(left, p1.y)
+            self.size = right - left + 1
+        elif p1.x < p2.x and p1.y < p2.y:
+            self.dir = 2 # South-east
+            self.p = Point(p1.x, p1.y)
+            self.size = p2.x - p1.x + 1
+        elif p1.x > p2.x and p1.y > p2.y:
+            self.dir = 2 # South-east
+            self.p = Point(p2.x, p2.y)
+            self.size = p1.x - p2.x + 1
+        elif p1.x < p2.x and p1.y > p2.y:
+            self.dir = 0 # North-east
+            self.p = Point(p1.x, p1.y)
+            self.size = p2.x - p1.x + 1
+        elif p1.x > p2.x and p1.y < p2.y:
+            self.dir = 0 # North-east
+            self.p = Point(p2.x, p2.y)
+            self.size = p1.x - p2.x + 1
         else:
-            raise Exception("Can only handle vertical or horizontal line segments.")
-
-    def overlap(self, other):
-        (c1, f1, l1, h1) = self.box()
-        (c2, f2, l2, h2) = other.box()
-        if c1 != c2:
-            if l1 <= f2 and h1 >= f2 and l2 <= f1 and h2 >= f1:
-                return {Point(f1 if c1 == 0 else f2, f1 if c1 == 1 else f2)}
-            else:
-                return set()
-        elif f1 == f2:
-            if l1 <= l2 and h1 >= l2 and h1 <= h2:
-                zs = range(l2, h1 + 1)
-            elif l2 <= l1 and h2 >= l1 and h2 <= h1:
-                zs = range(l1, h2 + 1)
-            elif l1 <= l2 and h1 >= h2:
-                zs = range(l2, h2 + 1)
-            elif l2 <= l1 and h2 >= h1:
-                zs = range(l1, h1 + 1)
-            else:
-                return set()
-            if c1 == 0:
-                return set(Point(f1, y) for y in zs)
-            else:
-                return set(Point(x, f1) for x in zs)     
-        else:
-            return set()
+            raise Exception("Unhandled case.")
+    
+    def draw(self):
+        match self.dir:
+            case 0: return {Point(self.p.x + t, self.p.y - t) for t in range(self.size)}
+            case 1: return {Point(self.p.x + t, self.p.y) for t in range(self.size)}
+            case 2: return {Point(self.p.x + t, self.p.y + t) for t in range(self.size)}
+            case 3: return {Point(self.p.x, self.p.y + t) for t in range(self.size)}
 
     def __str__(self) -> str:
-        return "{} -> {}".format(self.p1, self.p2)
+        return "d: {}, p: {}, size: {}".format(self.dir, self.p, self.size)
 
 def read(file: str) -> list[LineSegment]:
     f = open(file, "r")
@@ -77,14 +76,17 @@ def read(file: str) -> list[LineSegment]:
     return segments
 
 def compute(data: list[LineSegment]):
-    c = set()
-    filtered = [s for s in data if s.p1.x == s.p2.x or s.p1.y == s.p2.y]
-    n = len(filtered)
-    for i in range(n):
-        for j in range(i+1,n):
-            c.update(filtered[i].overlap(filtered[j]))
-    return len(c)
+    floor = dict()
+    for s in data:
+        for p in s.draw():
+            if p in floor:
+                floor[p] += 1
+            else:
+                floor[p] = 1
+    return sum(1 for (p, v) in floor.items() if v > 1)
 
 data = read(file)
+filtered = [s for s in data if s.dir == 1 or s.dir == 3]
 
-print("Dec 5, part 1: {}".format(compute(data)))
+print("Dec 5, part 1: {}".format(compute(filtered)))
+print("Dec 5, part 2: {}".format(compute(data)))
