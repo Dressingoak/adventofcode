f72e0519d9988940621ce4f227f59f0e0a19144e
Andreas Jessen <dressingoak@gmail.com>
2021-12-07 06:36:57 +0100

    Solution to Dec 7, part 2

--- 2021/dec7/solution.py
+++ 2021/dec7/solution.py
@@ -17,6 +17,20 @@ def compute(data: list[int]):
         tries.append(sum(abs(j - i) for j in data))
     return min(tries)
 
+def compute_with_growing_cost(data: list[int]):
+    least = min(data)
+    greatest = max(data)
+    data.sort()
+    tries = []
+    for i in range(least, greatest + 1):
+        s = 0
+        for j in data:
+            d = abs(j - i)
+            s += d * (d + 1) // 2
+        tries.append(s)
+    return min(tries)
+
 data = read(file)
 
 print("Dec 6, part 1: {}".format(compute(data)))
+print("Dec 6, part 2: {}".format(compute_with_growing_cost(data)))
