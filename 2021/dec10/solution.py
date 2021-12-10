50161cf758014567c9128db42d8fdb4aefbe53f4
Andreas Jessen <dressingoak@gmail.com>
2021-12-10 09:20:53 +0100

    Solution to Dec 10, part 2

--- 2021/dec10/solution.py
+++ 2021/dec10/solution.py
@@ -20,12 +20,27 @@ def validate(line: str):
             if char_pairs[builder[-1]] == char:
                 builder = builder[:-1]
             else:
-                return scores[char]
-    return 0
+                return (builder, scores[char])
+    return (builder, 0)
 
 def validate_all(lines: list[str]):
-    return sum(validate(line) for line in lines)
+    return sum(validate(line)[1] for line in lines)
+
+def autocomplete(line: str):
+    scores = {"(": 1, "[": 2, "{": 3, "<": 4}
+    score = 0
+    for char in reversed(line):
+        score *= 5
+        score += scores[char]
+    return score
+
+def autocomplete_all(lines: list[str]):
+    incomplete = [built for (built, score) in map(validate, lines) if score == 0]
+    scores = list(map(autocomplete, incomplete))
+    scores.sort()
+    return scores[len(scores) // 2]
 
 data = read(file)
 
-print("Dec 9, part 1: {}".format(validate_all(data)))
+print("Dec 10, part 1: {}".format(validate_all(data)))
+print("Dec 10, part 2: {}".format(autocomplete_all(data)))
