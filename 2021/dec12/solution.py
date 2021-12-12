c28179193c871e838d17cdae35c390b7a16a4cd3
Andreas Jessen <dressingoak@gmail.com>
2021-12-12 15:05:52 +0100

    Solution to Dec 12, part 2

--- 2021/dec12/solution.py
+++ 2021/dec12/solution.py
@@ -22,20 +22,24 @@ def connections(data: list[tuple[str, str]]):
             g[r].add(l)
     return g
 
-def paths(g: dict[str, set[str]], c: str = "start", s: list[str] = ["start"]):
+def paths(g: dict[str, set[str]], visited: bool, c: str = "start", s: list[str] = ["start"]):
     if c == "end":
         return 1
     p = 0
     for e in g[c]:
-        if e in s:
+        v = visited
+        if e in s and (visited or e == "start"):
             continue
+        if e in s and not visited:
+            v = True
         sc = [_ for _ in s]
         if e.islower():
             sc.append(e)
-        p += paths(g, e, sc)
+        p += paths(g, v, e, sc)
     return p
 
 data = read(file)
 g = connections(data)
 
-print("Dec 12, part 1: {}".format(paths(g)))
+print("Dec 12, part 1: {}".format(paths(g, True)))
+print("Dec 12, part 2: {}".format(paths(g, False)))
