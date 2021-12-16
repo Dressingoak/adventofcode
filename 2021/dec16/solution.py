c4ecf83b55c1cff5cee4ff30db1805a3f7e7be7d
Andreas Jessen <dressingoak@gmail.com>
2021-12-17 00:13:53 +0100

    Solution to Dec 16, part 2

--- 2021/dec16/solution.py
+++ 2021/dec16/solution.py
@@ -24,39 +24,56 @@ def decode(bits: list[bool], c: int):
     if type == 4:
         r = True
         i = c + 6
-        # data = []
+        data = []
         while r:
-            # data += bits[i + 1:i + 5]
+            data += bits[i + 1:i + 5]
             r = bits[i]
             i += 5
-        return i - c, version
-        # return [version, type, i - c, binary_to_int(data, 0, len(data))]
+        return i - c, version, binary_to_int(data, 0, len(data))
     else:
         length_type = bits[c + 6]
         i = c + 7
+        values = []
         if not length_type: # length type ID: 0
             size = binary_to_int(bits, i, i + 15)
             i += 15
             acc = 0
             while acc < size:
-                s, v = decode(bits, i)
+                s, v, e = decode(bits, i)
                 acc += s
                 version += v
                 i += s
-            return i - c, version
+                values.append(e)
         else: # length type ID: 1
             count = binary_to_int(bits, i, i + 11)
             i += 11
             for _ in range(count):
-                s, v = decode(bits, i)
+                s, v, e = decode(bits, i)
                 version += v
                 i += s
-            return i - c, version
+                values.append(e)
+        match type:
+            case 0: eval = sum(values)
+            case 1:
+                eval = 1
+                for value in values:
+                    eval *= value
+            case 2: eval = min(values)
+            case 3: eval = max(values)
+            case 5: eval = int(values[0] > values[1])
+            case 6: eval = int(values[0] < values[1])
+            case 7: eval = int(values[0] == values[1])
+        return i - c, version, eval
 
-def compute(bits: list[bool]) -> int:
-    _, s = decode(bits, 0)
+def compute_version_sum(bits: list[bool]) -> int:
+    _, s, _ = decode(bits, 0)
     return s
+
+def compute_evaluation(bits: list[bool]) -> int:
+    _, _, e = decode(bits, 0)
+    return e
 
 data = read(file)
 
-print("Dec 16, part 1: {}".format(compute(data)))
+print("Dec 16, part 1: {}".format(compute_version_sum(data)))
+print("Dec 16, part 2: {}".format(compute_evaluation(data)))
