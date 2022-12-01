cbc7544e430f309e22c2f12312b65c0b1c603bf5
Andreas Jessen <dressingoak@gmail.com>
2022-12-01 18:20:48 +0100

    Solution to Dec 1, part 2

--- 2022/dec1/solution.py
+++ 2022/dec1/solution.py
@@ -1,21 +1,41 @@
 import sys
 
-try:
-    file = sys.argv[1]
-except:
-    file = "input.txt"
-
-def calculate(file: str):
+def calculate_part1(file: str):
     cur, max = 0, 0
-    f = open(file, "r")
-    for line in f.readlines():
-        match line.strip():
-            case "":
-                if cur > max:
-                    max = cur
-                cur = 0
-            case x:
-                cur += int(x)
+    with open(file, "r") as f:
+        for line in f.readlines():
+            match line.strip():
+                case "":
+                    if cur > max:
+                        max = cur
+                    cur = 0
+                case x:
+                    cur += int(x)
+        if cur > max:
+            max = cur
     return max
 
-print("Dec 1, part 1: {}".format(calculate(file)))
+def calculate_part2(file: str):
+    cur, l = 0, []
+    with open(file, "r") as f:
+        for line in f.readlines():
+            match line.strip():
+                case "":
+                    l.append(cur)
+                    cur = 0
+                case x:
+                    cur += int(x)
+        if cur > 0:
+            l.append(cur)
+    l.sort(reverse=True)
+    return sum(l[:3])
+
+    
+if __name__ == '__main__':
+    try:
+        file = sys.argv[1]
+    except:
+        file = "input.txt"
+
+    print("Dec 1, part 1: {}".format(calculate_part1(file)))
+    print("Dec 1, part 2: {}".format(calculate_part2(file)))
