bee33897e5f54f7edd61d7071411720206aaea40
Andreas Jessen <dressingoak@gmail.com>
2022-12-04 09:15:55 +0100

    Solution to Dec 4, part 2

--- 2022/dec4/solution.py
+++ 2022/dec4/solution.py
@@ -4,19 +4,39 @@ def to_range(lst: str) -> tuple[int, int]:
     l, r = lst.split("-")
     return (int(l), int(r))
 
+def overlaps_fully(e1, e2, flip=False):
+    if e1[0] <= e2[0] and e1[1] >= e2[1]:
+        return 1
+    elif flip:
+        return overlaps_fully(e2, e1, False)
+    else:
+        return 0
+
+def overlaps_partially(e1, e2, flip=False):
+    if overlaps_fully(e1, e2, False) == 1:
+        return 1
+    elif e1[0] <= e2[0] and e1[1] >= e2[0] and e1[1] < e2[1]:
+        return 1
+    elif flip:
+        return overlaps_partially(e2, e1, False)
+    else:
+        return 0
+
 def calculate_part1(file: str):
     s = 0
     with open(file, "r") as f:
         for line in f.readlines():
             e1, e2 = [to_range(_) for _ in line.strip().split(",")]
-            if (e1[0] <= e2[0] and e1[1] >= e2[1]) or (e2[0] <= e1[0] and e2[1] >= e1[1]):
-                s += 1
+            s += overlaps_fully(e1, e2, True)
     return s
 
-# def calculate_part2(file: str):
-#     with open(file, "r") as f:
-#         pass
-#     return 0
+def calculate_part2(file: str):
+    s = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            e1, e2 = [to_range(_) for _ in line.strip().split(",")]
+            s += overlaps_partially(e1, e2, True)
+    return s
     
 if __name__ == '__main__':
     try:
@@ -25,4 +45,4 @@ if __name__ == '__main__':
         file = "input.txt"
 
     print("Dec 4, part 1: {}".format(calculate_part1(file)))
-    # print("Dec 4, part 2: {}".format(calculate_part2(file)))
+    print("Dec 4, part 2: {}".format(calculate_part2(file)))
