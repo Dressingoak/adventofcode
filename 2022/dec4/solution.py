2d0cc536f0f6fd9e30d12492210337e38f802fbe
Andreas Jessen <dressingoak@gmail.com>
2022-12-04 09:30:50 +0100

    Rewrote Dec 4 to use Python's structural pattern matching instead of if-statements

--- 2022/dec4/solution.py
+++ 2022/dec4/solution.py
@@ -4,30 +4,25 @@ def to_range(lst: str) -> tuple[int, int]:
     l, r = lst.split("-")
     return (int(l), int(r))
 
-def overlaps_fully(e1, e2, flip=False):
-    if e1[0] <= e2[0] and e1[1] >= e2[1]:
-        return 1
-    elif flip:
-        return overlaps_fully(e2, e1, False)
-    else:
-        return 0
+def overlaps_fully(e1, e2, flip=True):
+    match (e1, e2, flip):
+        case ((l1, l2), (r1, r2), _) if l1 <= r1 and l2 >= r2: return True
+        case (l, r, True): return overlaps_fully(r, l, False)
+        case _: return False
 
-def overlaps_partially(e1, e2, flip=False):
-    if overlaps_fully(e1, e2, False) == 1:
-        return 1
-    elif e1[0] <= e2[0] and e1[1] >= e2[0] and e1[1] < e2[1]:
-        return 1
-    elif flip:
-        return overlaps_partially(e2, e1, False)
-    else:
-        return 0
+def overlaps_partially(e1, e2, flip=True):
+    match (e1, e2, flip):
+        case (l, r, _) if overlaps_fully(l, r, False): return True
+        case ((l1, l2), (r1, r2), _) if l1 <= r1 and l2 >= r1 and l2 < r2: return True
+        case (l, r, True): return overlaps_partially(r, l, False)
+        case _: return False
 
 def calculate_part1(file: str):
     s = 0
     with open(file, "r") as f:
         for line in f.readlines():
             e1, e2 = [to_range(_) for _ in line.strip().split(",")]
-            s += overlaps_fully(e1, e2, True)
+            s += int(overlaps_fully(e1, e2))
     return s
 
 def calculate_part2(file: str):
@@ -35,7 +30,7 @@ def calculate_part2(file: str):
     with open(file, "r") as f:
         for line in f.readlines():
             e1, e2 = [to_range(_) for _ in line.strip().split(",")]
-            s += overlaps_partially(e1, e2, True)
+            s += int(overlaps_partially(e1, e2))
     return s
     
 if __name__ == '__main__':
