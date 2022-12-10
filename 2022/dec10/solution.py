52562e056e934e6083d8d5cb3f4833979a794d6d
Andreas Jessen <dressingoak@gmail.com>
2022-12-10 09:37:30 +0100

    Solution to Dec 10, part 1

---
+++ 2022/dec10/solution.py
@@ -0,0 +1,32 @@
+import sys
+
+def cpu(file: str):
+    cycle = 0
+    X = 1
+    with open(file, "r") as f:
+        for line in f.readlines():
+            cycle += 1
+            yield (cycle, X)
+            match line.strip().split():
+                case ["noop"]: pass
+                case ["addx", v]:
+                    cycle += 1
+                    yield (cycle, X)
+                    X += int(v)
+
+def calculate_part1(file: str):
+    return sum(cycle * X for cycle, X in cpu(file) if cycle % 40 - 20 == 0)
+
+# def calculate_part2(file: str):
+#     with open(file, "r") as f:
+#         pass
+#     return 0
+    
+if __name__ == '__main__':
+    try:
+        file = sys.argv[1]
+    except:
+        file = "input.txt"
+
+    print("Dec 10, part 1: {}".format(calculate_part1(file)))
+    # print("Dec 10, part 2: {}".format(calculate_part2(file)))
