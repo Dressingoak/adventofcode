9a67ba9c384cc3f3075a96331661958275361198
Andreas Jessen <dressingoak@gmail.com>
2022-12-14 09:47:07 +0100

    Rearranged functions

--- 2022/dec14/solution.py
+++ 2022/dec14/solution.py
@@ -1,5 +1,44 @@
 import sys
 
+def parse_cave(file: str):
+    cave = {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            prev = None
+            for coords in line.strip().split(" -> "):
+                match coords.split(","):
+                    case [x1, y1]:
+                        x1, y1 = int(x1), int(y1)
+                        match prev:
+                            case (x, y0) if x == x1:
+                                for y in range(min(y0, y1), max(y0, y1)+1):
+                                    if x in cave:
+                                        cave[x][y] = "#"
+                                    else:
+                                        cave[x] = {y: "#"}
+                            case (x0, y) if y == y1:
+                                for x in range(min(x0, x1), max(x0, x1)+1):
+                                    if x in cave:
+                                        cave[x][y] = "#"
+                                    else:
+                                        cave[x] = {y: "#"}
+                        prev = (x1, y1)
+    return cave
+
+def show_cave(cave: dict[int, dict[int, str]]):
+    x0, x1 = min(cave.keys()), max(cave.keys())
+    y0 = min(y for ys in cave.values() for y in ys.keys())
+    y1 = max(y for ys in cave.values() for y in ys.keys())
+    s = ""
+    for y in range(y0 - 1, y1 + 2):
+        for x in range(x0 - 1, x1 + 2):
+            if x in cave and y in cave[x]:
+                s += cave[x][y]
+            else:
+                s += "."
+        s += "\n"
+    return s.strip()
+
 def simulate(cave: dict[int, dict[int, str]], cur: tuple[int, int] | None = None):
     match cur:
         case None: # Source sand from starting position
@@ -23,44 +62,8 @@ def simulate(cave: dict[int, dict[int, str]], cur: tuple[int, int] | None = None
                     return None
                 return (x, highest - 1) # Sand is at rest here
 
-def show_cave(cave: dict[int, dict[int, str]]):
-    x0, x1 = min(cave.keys()), max(cave.keys())
-    y0 = min(y for ys in cave.values() for y in ys.keys())
-    y1 = max(y for ys in cave.values() for y in ys.keys())
-    s = ""
-    for y in range(y0 - 1, y1 + 2):
-        for x in range(x0 - 1, x1 + 2):
-            if x in cave and y in cave[x]:
-                s += cave[x][y]
-            else:
-                s += "."
-        s += "\n"
-    return s.strip()
-        
-
 def calculate_part1(file: str, show=False):
-    cave = {}
-    with open(file, "r") as f:
-        for line in f.readlines():
-            prev = None
-            for coords in line.strip().split(" -> "):
-                match coords.split(","):
-                    case [x1, y1]:
-                        x1, y1 = int(x1), int(y1)
-                        match prev:
-                            case (x, y0) if x == x1:
-                                for y in range(min(y0, y1), max(y0, y1)+1):
-                                    if x in cave:
-                                        cave[x][y] = "#"
-                                    else:
-                                        cave[x] = {y: "#"}
-                            case (x0, y) if y == y1:
-                                for x in range(min(x0, x1), max(x0, x1)+1):
-                                    if x in cave:
-                                        cave[x][y] = "#"
-                                    else:
-                                        cave[x] = {y: "#"}
-                        prev = (x1, y1)
+    cave = parse_cave(file)
     c = 0
     while True:
         match simulate(cave):
