dd4330405f020c0f134a27e5abd0fa4fc70cb8f5
Andreas Jessen <dressingoak@gmail.com>
2022-12-14 13:03:00 +0100

    Solution to Dec 11, part 2

--- 2022/dec11/solution.py
+++ 2022/dec11/solution.py
@@ -1,25 +1,7 @@
 import sys
 import re
 
-def combine(a, b):
-    d = {k: v for k, v in a.items()}
-    for k, v in b.items():
-        if k in d:
-            d[k] += v
-        else:
-            d[k] = v
-    return d
-
-def increment(a, kk):
-    d = {k: v for k, v in a.items()}
-    if kk not in d:
-        d[kk] = 0
-    d[kk] += 1
-    return d
-
 class Monkey:
-    relief = None
-
     def __init__(self, items, op, test, true, false):
         self.items = items
         self.op = op
@@ -32,7 +14,7 @@ class Monkey:
         self.inspections += len(self.items)
         for item in self.items:
             new = self.op(item)
-            new //= self.relief
+            new //= 3
             if new % self.test == 0:
                 monkeys[self.true].catch(new)
             else:
@@ -41,16 +23,6 @@ class Monkey:
 
     def catch(self, item):
         self.items.append(item)
-
-    def throw2(self, monkeys, id, value, turns, known = {}):
-        # print(id, value, turns)
-        if turns == 0:
-            return known
-        next_value = self.op(value)
-        next_known = increment(known, id)
-        next_id = self.true if next_value % self.test == 0 else self.false
-        next_turn = turns if next_id > id else turns - 1
-        return monkeys[next_id].throw2(monkeys, next_id, next_value, next_turn, next_known)
 
 def parse_op(op, rhs, mod=None):
     match (op, rhs):
@@ -81,7 +53,6 @@ def parse(file: str, mod=False):
     return monkeys
 
 def calculate_part1(file: str):
-    Monkey.relief = 3
     monkeys = parse(file)
     for _ in range(20):
         for i in range(len(monkeys)):
@@ -92,33 +63,36 @@ def calculate_part1(file: str):
         case [a, b, *_]: return a * b
 
 def calculate_part2(file: str):
-    Monkey.relief = 1
     monkeys = parse(file)
-    for _ in range(2):
-        for i in range(len(monkeys)):
-            monkeys[i].throw(monkeys)
+    ops, mods, maps, inits, inspections = {}, {}, {}, [], {}
     for i, monkey in enumerate(monkeys):
-        print(f"Monkey {i}: {monkey.inspections}")
-    inspections = [monkey.inspections for monkey in monkeys]
-    inspections.sort(reverse=True)
-    match inspections:
-        case [a, b, *_]: return a * b
+        ops[i] = monkey.op
+        mods[i] = monkey.test
+        maps[i] = (monkey.true, monkey.false)
+        inits.extend([(i, init) for init in monkey.items])
+        inspections[i] = 0
 
-def calculate_part2b(file: str):
-    Monkey.relief = 1
-    monkeys = parse(file)
-    d = {}
-    for id, monkey in enumerate(monkeys):
-        for item in monkey.items:
-            # print(id, item)
-            # itemz = monkey.op(item % monkey.test) % monkey.test
-            d = combine(d, monkey.throw2(monkeys, id, item, 700))
-    monkey_inspections = sorted((k, v) for k, v in d.items())
-    for i, inspections in monkey_inspections:
-        print(f"Monkey {i}: {inspections}")
-    inspections = [inspections for (_, inspections) in monkey_inspections]
-    inspections.sort(reverse=True)
-    match inspections:
+    def get(i, prev):
+        next = {j: ops[i](prev[j]) % mods[j] for j in prev.keys()}
+        k = maps[i][0] if next[i] == 0 else maps[i][1]
+        return k, next
+
+    def yielder(start, init, r_max):
+        i = start
+        prev = {j: init % mods[j] for j in mods.keys()}
+        r = 1
+        while r <= r_max:
+            yield i
+            k, prev = get(i, prev)
+            if k <= i:
+                r += 1
+            i = k
+
+    for (i, init) in inits:
+        for j in yielder(i, init, 10000):
+            inspections[j] += 1
+
+    match sorted(inspections.values(), reverse=True):
         case [a, b, *_]: return a * b
     
 if __name__ == '__main__':
@@ -128,6 +102,4 @@ if __name__ == '__main__':
         file = "input.txt"
 
     print("Dec 11, part 1: {}".format(calculate_part1(file)))
-
     print("Dec 11, part 2: {}".format(calculate_part2(file)))
-    print("Dec 11, part 2b: {}".format(calculate_part2b(file)))
