0a6b3a4f11b266a2acb0130168e58d887f51c0c7
Andreas Jessen <dressingoak@gmail.com>
2022-12-14 08:17:57 +0100

    Solution to Dec 13, part 2

--- 2022/dec13/solution.py
+++ 2022/dec13/solution.py
@@ -1,6 +1,6 @@
 import sys
 
-def parse_packet(packet: str):
+def parse_packet(packet: str) -> list:
     i, l = 1, []
     num = ""
     while i < len(packet):
@@ -27,7 +27,7 @@ def parse_packet(packet: str):
             num += packet[i]
             i+=1
 
-def parse_packets(file: str):
+def parse_packets(file: str) -> list[tuple[list, list]]:
     pairs = []
     with open(file, "r") as f:
         pair = []
@@ -67,18 +67,34 @@ def compare(left, right):
     elif len(left) > len(right):
         return False
 
+def sorter(lst: list, gt) -> list:
+    n = len(lst)
+    for i in range(n):
+        is_sorted = True
+        for j in range(n - i - 1):
+            if gt(lst[j], lst[j+1]):
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                is_sorted = False
+        if is_sorted:
+            break
+
 def calculate_part1(file: str):
     packets = parse_packets(file)
-    s = 0
-    for i, (left, right) in enumerate(packets):
-        if compare(left, right):
-            s += i+1
-    return s
+    return sum(i+1 for i, (left, right) in enumerate(packets) if compare(left, right))
 
-# def calculate_part2(file: str):
-#     with open(file, "r") as f:
-#         pass
-#     return 0
+def calculate_part2(file: str):
+    packets = parse_packets(file)
+    decoders: set[list] = [[[2]], [[6]]]
+    all_packets = [_ for _ in decoders]
+    for pair in packets:
+        match pair:
+            case (l, r): all_packets.extend([l, r])
+    sorter(all_packets, lambda x, y: compare(y, x))
+    p = 1
+    for i, packet in enumerate(all_packets):
+        if any(compare(packet, decoder) is None for decoder in decoders):
+            p *= i+1
+    return p
     
 if __name__ == '__main__':
     try:
@@ -87,4 +103,4 @@ if __name__ == '__main__':
         file = "input.txt"
 
     print("Dec 13, part 1: {}".format(calculate_part1(file)))
-    # print("Dec 13, part 2: {}".format(calculate_part2(file)))
+    print("Dec 13, part 2: {}".format(calculate_part2(file)))
