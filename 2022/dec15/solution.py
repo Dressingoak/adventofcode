eb7a930feb0930c344fb22a285b6844bf9a5220a
Andreas Jessen <dressingoak@gmail.com>
2022-12-15 09:36:50 +0100

    Separated parser

--- 2022/dec15/solution.py
+++ 2022/dec15/solution.py
@@ -1,18 +1,23 @@
 import sys
 import re
 
-def calculate_part1(file: str, row):
-    occupied, beacons = set(), set()
+def parse(file: str):
+    pattern = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
     with open(file, "r") as f:
         for line in f.readlines():
-            m = re.match(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)", line.strip())
+            m = re.match(pattern, line.strip())
             sx, sy, bx, by = [int(_) for _ in m.group(1, 2, 3, 4)]
-            if by == row:
-                beacons.add(bx)
-            dist_to_b = abs(bx - sx) + abs(by - sy)
-            if row >= sy - dist_to_b and row <= sy + dist_to_b:
-                dist_to_r = dist_to_b - abs(sy - row)
-                occupied.update(_ for _ in range(sx - dist_to_r, sx + dist_to_r + 1))
+            yield sx, sy, bx, by
+
+def calculate_part1(file: str, row):
+    occupied, beacons = set(), set()
+    for sx, sy, bx, by in parse(file):
+        if by == row:
+            beacons.add(bx)
+        dist_to_b = abs(bx - sx) + abs(by - sy)
+        if row >= sy - dist_to_b and row <= sy + dist_to_b:
+            dist_to_r = dist_to_b - abs(sy - row)
+            occupied.update(_ for _ in range(sx - dist_to_r, sx + dist_to_r + 1))
     occupied.difference_update(beacons)
     return len(occupied)
 
