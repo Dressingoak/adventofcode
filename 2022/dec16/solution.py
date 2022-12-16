8259ae54bc41e52ee8fdbdceba6aea464234acbd
Andreas Jessen <dressingoak@gmail.com>
2022-12-17 00:18:48 +0100

    A lot faster solution to Dec 16, part 2 (still a bit slow though)

--- 2022/dec16/solution.py
+++ 2022/dec16/solution.py
@@ -92,65 +92,64 @@ def get_connection_costs(file: str, start_valve: str):
 def restrict_connections(connection_costs, start_valve, allowed_connections):
     return {k: {kk: vv for kk, vv in v.items() if kk in allowed_connections and kk != start_valve} for k, v in connection_costs.items() if k in allowed_connections or k == start_valve}
 
-def move(current_valve, time_remaining, pressure, connection_costs):
+def create_key(current_valve: str, visited_valves: set[str], time_remaining: int):
+    return "".join(sorted([_ for _ in visited_valves])) + current_valve + "-" +  str(time_remaining)
+
+def build_connections(current_valve: str, visited_valves: set[str], time_remaining: int, connection_costs: dict[str, dict[str, tuple[int, int]]], graph: dict[(str, int), dict[(str, int), int]]):
     next_valves = {valve: (time_remaining - travel_cost - 1, flow_rate) for valve, (travel_cost, flow_rate) in connection_costs[current_valve].items()}
-    next_valves = {valve: (next_time_remaining, pressure + next_time_remaining * flow_rate) for valve, (next_time_remaining, flow_rate) in next_valves.items() if next_time_remaining > 0}
+    next_valves = {valve: (next_time_remaining, next_time_remaining * flow_rate) for valve, (next_time_remaining, flow_rate) in next_valves.items() if next_time_remaining > 0}
+    src = create_key(current_valve, visited_valves, time_remaining)
     if len(next_valves) == 0:
-        yield pressure
+        graph[src] = {}
+        return
     else:
+        adj = {}
+        next = []
         for next_valve, (next_time_remaining, next_pressure) in next_valves.items():
-            next_connection_costs = restrict_connections(connection_costs, next_valve, next_valves.keys())
-            yield from move(next_valve, next_time_remaining, next_pressure, next_connection_costs)
-
-def combination(arr, n, r, index, data, i):
-    if(index == r):
-        yield data[:r]
-    elif(i < n):
-        data[index] = arr[i]
-        yield from combination(arr, n, r, index + 1, data, i + 1)
-        yield from combination(arr, n, r, index, data, i + 1)
-
-def subsets(arr, size):
-    n = len(arr)
-    data = list(range(size))
-    yield from combination(arr, n, size, 0, data, 0)
-
-def unique_splits_half(arr):
-    n = len(arr)
-    for i in range(n // 2 + 1):
-        if i == n // 2 and n % 2 == 0:
-            arrs = [subset for subset in subsets(arr, i)]
-            m = len(arrs) // 2
-            yield from arrs[:m]
-        else:
-            yield from subsets(arr, i)
-
-def unique_splits(arr):
-    arr_set = set(arr)
-    for split in unique_splits_half(arr):
-        left = set(split)
-        right = arr_set.difference(left)
-        yield (left, right)
-
-def distribute_connections(connection_costs, start_valve):
-    valves = [valve for valve in connection_costs.keys() if valve != start_valve]
-    for left, right in unique_splits(valves):
-        yield (restrict_connections(connection_costs, start_valve, left), restrict_connections(connection_costs, start_valve, right))
+            next_visited_valves = visited_valves.union({current_valve})
+            dst = create_key(next_valve, next_visited_valves, next_time_remaining)
+            if not dst in graph:
+                next.append((next_valve, next_visited_valves, next_time_remaining))
+            adj[dst] = -next_pressure
+        graph[src] = adj
+        for next_valve, next_visited_valves, next_time_remaining in next:
+            build_connections(next_valve, next_visited_valves, next_time_remaining, restrict_connections(connection_costs, next_valve, next_valves.keys()), graph)
 
 def calculate_part1(file: str):
     start_valve = "AA"
     connection_costs = get_connection_costs(file, start_valve)
-    return max(move(start_valve, 30, 0, connection_costs))
+    graph = {}
+    build_connections(start_valve, set(), 30, connection_costs, graph)
+    _, pressures = dijkstra(graph, create_key(start_valve, set(), 30), None)
+    maximal = -min(pressures.values())
+    return maximal
 
 def calculate_part2(file: str):
     start_valve = "AA"
     connection_costs = get_connection_costs(file, start_valve)
-    m = 0
-    # total = sum(1 for _ in unique_splits_half([valve for valve in connection_costs.keys() if valve != start_valve]))
-    for i, (left, right) in enumerate(distribute_connections(connection_costs, start_valve)):
-        m = max(m, max(move(start_valve, 26, 0, left)) + max(move(start_valve, 26, 0, right)))
-        # print(f"progress: {i+1} / {total}")
-    return m
+    graph = {}
+    build_connections(start_valve, set(), 26, connection_costs, graph)
+    _, pressures = dijkstra(graph, create_key(start_valve, set(), 26), None)
+    maximals = {}
+    for k, v in pressures.items():
+        valves = k.split("-")[0]
+        valves_key = "".join(sorted([valves[i:i+2] for i in range(0, len(valves), 2) if valves[i:i+2] != start_valve]))
+        if valves_key in maximals:
+            maximals[valves_key] = max(maximals[valves_key], -v)
+        else:
+            maximals[valves_key] = -v
+    maximal = 0
+    for left, v1 in maximals.items():
+        left_len = len(left)
+        left_valves = set(left[i:i+2] for i in range(0, len(left), 2) if left[i:i+2])
+        for right, v2 in maximals.items():
+            right_len = len(right)
+            if right_len > left_len: # To skip some checks
+                continue
+            right_valves = set(right[i:i+2] for i in range(0, len(right), 2) if right[i:i+2])
+            if len(left_valves.intersection(right_valves)) == 0:
+                maximal = max(maximal, v1 + v2)
+    return maximal
 
 if __name__ == '__main__':
     try:
