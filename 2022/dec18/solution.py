6794d0845239e48f3be51f7c2a9b961471cab37c
Andreas Jessen <dressingoak@gmail.com>
2022-12-18 11:34:15 +0100

    Merged solution for part 1 and 2 into same method

--- 2022/dec18/solution.py
+++ 2022/dec18/solution.py
@@ -1,70 +1,60 @@
 import sys
 from path_finding import dijkstra
 
-def calculate_part1(file: str):
-    sides = 0
-    adj = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
-    seen = set()
-    with open(file, "r") as f:
-        for line in f.readlines():
-            match line.strip().split(","):
-                case [x, y, z]:
-                    x, y, z = int(x), int(y), int(z)
-                    sides += 6
-                    for (dx, dy, dz) in adj:
-                        if (x + dx, y + dy, z + dz) in seen:
-                            sides -= 2
-                    seen.add((x, y, z))
-    return sides
+Cube = tuple[int, int, int]
 
-def calculate_part2(file: str):
+adj: list[Cube] = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+
+def add_sides(cube: Cube, seen: set[Cube]) -> int:
+    sides = 0
+    match cube:
+        case (x, y, z):
+            sides += 6
+            for (dx, dy, dz) in adj:
+                if (x + dx, y + dy, z + dz) in seen:
+                    sides -= 2
+            seen.add((x, y, z))
+            return sides
+
+def count_sides(file: str, with_interior: bool):
     sides = 0
     ds = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
     seen = set()
     with open(file, "r") as f:
         for line in f.readlines():
             match line.strip().split(","):
-                case [x, y, z]:
-                    x, y, z = int(x), int(y), int(z)
-                    sides += 6
+                case [x, y, z]: sides += add_sides((int(x), int(y), int(z)), seen)
+    if not with_interior:
+        xmin, xmax, ymin, ymax, zmin, zmax = 0, 0, 0, 0, 0, 0
+        for cube in seen:
+            match cube:
+                case (x, y, z):
+                    xmin, xmax = min(xmin, x), max(xmax, x)
+                    ymin, ymax = min(ymin, y), max(ymax, y)
+                    zmin, zmax = min(zmin, z), max(zmax, z)
+        graph = {}
+        for x in range(xmin - 1, xmax + 2):
+            for y in range(ymin -1, ymax + 2):
+                for z in range(zmin -1, zmax + 2):
+                    if (x, y, z) in seen:
+                        continue
+                    adj = {}
                     for (dx, dy, dz) in ds:
-                        if (x + dx, y + dy, z + dz) in seen:
-                            sides -= 2
-                    seen.add((x, y, z))
-    xmin, xmax, ymin, ymax, zmin, zmax = 0, 0, 0, 0, 0, 0
-    for cube in seen:
-        match cube:
-            case (x, y, z):
-                xmin, xmax = min(xmin, x), max(xmax, x)
-                ymin, ymax = min(ymin, y), max(ymax, y)
-                zmin, zmax = min(zmin, z), max(zmax, z)
-    graph = {}
-    for x in range(xmin - 1, xmax + 2):
-        for y in range(ymin -1, ymax + 2):
-            for z in range(zmin -1, zmax + 2):
-                if (x, y, z) in seen:
-                    continue
-                adj = {}
-                for (dx, dy, dz) in ds:
-                    c = (x + dx, y + dy, z + dz)
-                    if c not in seen and c[0] >= xmin - 1 and c[0] <= xmax + 1 and c[1] >= ymin - 1 and c[1] <= ymax + 1 and c[2] >= zmin - 1 and c[2] <= zmax + 1:
-                        adj[c] = 1
-                graph[(x, y, z)] = adj
-    _, dist = dijkstra(graph, (-1, -1, -1), None)
-    interiour = {k for k, v in dist.items() if v is None}
-    for (x, y, z) in interiour:
-        sides += 6
-        for (dx, dy, dz) in ds:
-            if (x + dx, y + dy, z + dz) in seen:
-                sides -= 2
-        seen.add((x, y, z))
+                        c = (x + dx, y + dy, z + dz)
+                        if c not in seen and c[0] >= xmin - 1 and c[0] <= xmax + 1 and c[1] >= ymin - 1 and c[1] <= ymax + 1 and c[2] >= zmin - 1 and c[2] <= zmax + 1:
+                            adj[c] = 1
+                    graph[(x, y, z)] = adj
+        _, dist = dijkstra(graph, (-1, -1, -1), None)
+        interiour = {k for k, v in dist.items() if v is None}
+        for cube in interiour:
+            sides += add_sides(cube, seen)
     return sides
-    
+
 if __name__ == '__main__':
     try:
         file = sys.argv[1]
     except:
         file = "input.txt"
 
-    print("Dec 18, part 1: {}".format(calculate_part1(file)))
-    print("Dec 18, part 2: {}".format(calculate_part2(file)))
+    print("Dec 18, part 1: {}".format(count_sides(file, True)))
+    print("Dec 18, part 2: {}".format(count_sides(file, False)))
--- 2022/dec18/test.py
+++ 2022/dec18/test.py
@@ -1,15 +1,15 @@
 import unittest
-from solution import calculate_part1, calculate_part2;
+from solution import count_sides;
 
 class TestDec18(unittest.TestCase):
 
     file = "test.txt"
 
     def test_part1(self):
-        self.assertEqual(calculate_part1(self.file), 64)
+        self.assertEqual(count_sides(self.file, True), 64)
 
     def test_part2(self):
-        self.assertEqual(calculate_part2(self.file), 58)
+        self.assertEqual(count_sides(self.file, False), 58)
 
 if __name__ == '__main__':
     unittest.main()
