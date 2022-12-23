a8db03610d01390c7fbadbda8c2205f26207937b
Andreas Jessen <dressingoak@gmail.com>
2022-12-23 13:17:45 +0100

    Solution to Dec 15, part 2

--- 2022/dec15/solution.py
+++ 2022/dec15/solution.py
@@ -9,6 +9,39 @@ def parse(file: str):
             sx, sy, bx, by = [int(_) for _ in m.group(1, 2, 3, 4)]
             yield sx, sy, bx, by
 
+class Interval:
+    def __init__(self, l, r) -> None:
+        self.l, self.r = l, r
+
+    def __repr__(self) -> str:
+        match (self.l, self.r):
+            case (l, r) if l < r: return f"[{l}..={r}]"
+            case (l, r) if l == r: return f"{{{l}}}"
+
+    def try_yield(l, r):
+        if l <= r:
+            yield Interval(l, r)
+
+    def difference(self, other):
+        if isinstance(other, Interval):
+            match (self.l, self.r, other.l, other.r):
+                case (l1, r1, l2, r2) if r1 < l2: # 2 is non-overlapping to the left
+                    yield Interval(l1, r1)
+                case (l1, r1, l2, r2) if l1 > r2: # 2 is non-overlapping to the right
+                    yield Interval(l1, r1)
+                case (l1, r1, l2, r2) if l1 <= l2 and r1 >= r2: # 1 fully overlaps 2
+                    yield from Interval.try_yield(l1, l2 - 1)
+                    yield from Interval.try_yield(r2 + 1, r1)
+                case (l1, r1, l2, r2) if l2 <= l1 and r2 >= r1: pass  # 2 fully overlaps 1
+                case (l1, r1, l2, r2) if l1 <= l2 and r1 >= l2: # 1 and 2 partially overlaps to the right
+                    yield from Interval.try_yield(l1, l2 - 1)
+                case (l1, r1, l2, r2) if l2 <= l1 and r2 >= l1: # 1 and 2 partially overlaps to the left
+                    yield from Interval.try_yield(r2 + 1, r1)
+        elif isinstance(other, int):
+            yield from self.difference(Interval(other, other))
+        else:
+            raise NotImplementedError()
+
 def calculate_part1(file: str, row):
     occupied, beacons = set(), set()
     for sx, sy, bx, by in parse(file):
@@ -21,28 +54,26 @@ def calculate_part1(file: str, row):
     occupied.difference_update(beacons)
     return len(occupied)
 
-class Polygon:
-    def __init__(self, points):
-        self.points = points
-    
-    def from_sensor_and_beacon(sensor, beacon):
-        match (sensor, beacon):
-            case ((sx, sy), (bx, by)):
-                dist = abs(bx - sx) + abs(by - sy)
-                return Polygon([
-                    (sx, sy + dist),
-                    (sx + dist, sy)
-                    (sx, sy - dist)
-                    (sx - dist, sy)
-                ])
+def search(file: str, size: int):
+    sensors = [_ for _ in parse(file)]
+    for row in range(size):
+        available = [Interval(0, size)]
+        for sx, sy, bx, by in sensors:
+            if by == row:
+                available = [diff for interval in available for diff in interval.difference(bx)]
+            dist_to_b = abs(bx - sx) + abs(by - sy)
+            if row >= sy - dist_to_b and row <= sy + dist_to_b:
+                dist_to_r = dist_to_b - abs(sy - row)
+                span = Interval(sx - dist_to_r, sx + dist_to_r)
+                available = [diff for interval in available for diff in interval.difference(span)]
+        match available:
+            case []: continue
+            case [interval] if interval.l == interval.r: return (row, interval.l)
+            case _: raise Exception("Unhandled")
 
-    def diff(self, other):
-        
-
-# def calculate_part2(file: str):
-#     with open(file, "r") as f:
-#         pass
-#     return 0
+def calculate_part2(file: str, size: int):
+    (y, x) = search(file, size)
+    return 4_000_000 * x + y
     
 if __name__ == '__main__':
     try:
@@ -50,5 +81,5 @@ if __name__ == '__main__':
     except:
         file = "input.txt"
 
-    print("Dec 15, part 1: {}".format(calculate_part1(file, 2000000)))
-    # print("Dec 15, part 2: {}".format(calculate_part2(file)))
+    print("Dec 15, part 1: {}".format(calculate_part1(file, 2_000_000)))
+    print("Dec 15, part 2: {}".format(calculate_part2(file, 4_000_000)))
