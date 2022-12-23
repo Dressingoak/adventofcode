24eabaeab9da563fceae6c3b22740418cc33bf9e
Andreas Jessen <dressingoak@gmail.com>
2022-12-23 10:43:22 +0100

    Solution to Dec 23, part 2

--- 2022/dec23/solution.py
+++ 2022/dec23/solution.py
@@ -1,6 +1,6 @@
 import sys
 
-def calculate_part1(file: str):
+def elves(file: str, stop=None):
     elves: set[tuple[int, int]] = set()
     with open(file, "r") as f:
         for i, line in enumerate(f.readlines()):
@@ -15,11 +15,15 @@ def calculate_part1(file: str):
         [(-1,  1), (-1,  0), (-1, -1)], # W
         [( 1,  1), ( 1,  0), ( 1, -1)]  # E
     ]
-    for round in range(10):
+    round = 0
+    standing = 0
+    while standing < len(elves) and (stop is None or (stop is not None and round < stop)):
+        standing = 0
         proposals = {}
         for x, y in elves:
             if len(elves.intersection([(a, b) for a in range(x-1,x+2) for b in range(y-1,y+2)])) == 1:
                 proposals[(x, y)] = set([(x, y)])
+                standing += 1
                 continue
             moves = False
             for d in range(round, round+4):
@@ -33,6 +37,7 @@ def calculate_part1(file: str):
             if moves:
                 continue
             proposals[(x, y)] = set([(x, y)])
+            standing += 1
         next = set()
         for k, v in proposals.items():
             if len(v) == 1:
@@ -40,17 +45,21 @@ def calculate_part1(file: str):
             else:
                 next.update(v)
         elves = next
+        round += 1
     x, y = elves.pop()
     xmin, xmax, ymin, ymax = x, x, y, y
     for (x, y) in elves:
         xmin, xmax = min(xmin, x), max(xmax, x)
         ymin, ymax = min(ymin, y), max(ymax, y)
-    return (xmax - xmin + 1) * (ymax - ymin + 1) - len(elves) - 1
+    return round, (xmax - xmin + 1) * (ymax - ymin + 1) - len(elves) - 1
 
-# def calculate_part2(file: str):
-#     with open(file, "r") as f:
-#         pass
-#     return 0
+def calculate_part1(file: str):
+    _, free = elves(file, 10)
+    return free
+
+def calculate_part2(file: str):
+    rounds, _ = elves(file, None)
+    return rounds
     
 if __name__ == '__main__':
     try:
@@ -59,4 +68,4 @@ if __name__ == '__main__':
         file = "input.txt"
 
     print("Dec 23, part 1: {}".format(calculate_part1(file)))
-    # print("Dec 23, part 2: {}".format(calculate_part2(file)))
+    print("Dec 23, part 2: {}".format(calculate_part2(file)))
