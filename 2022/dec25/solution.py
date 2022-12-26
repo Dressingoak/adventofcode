40bc067dabb61777bf4d3f5c80b6c5795022469d
Andreas Jessen <dressingoak@gmail.com>
2022-12-26 08:55:55 +0100

    Solution to Dec 25, part 1

---
+++ 2022/dec25/solution.py
@@ -0,0 +1,40 @@
+import sys
+
+def snafu_to_decimal(x):
+    lst = []
+    for char in x:
+        match char:
+            case "0" | "1" | "2": lst.append(int(char))
+            case "-": lst.append(-1)
+            case "=": lst.append(-2)
+    return sum(5**i * v for i, v in enumerate(reversed(lst)))
+
+def gen_decimal_to_snafu(x):
+    if x > 0:
+        shifted = (x+2) % 5
+        match shifted:
+            case 0: yield "="
+            case 1: yield "-"
+            case 2: yield "0"
+            case 3: yield "1"
+            case 4: yield "2"
+        yield from gen_decimal_to_snafu((x+2-shifted) // 5)
+
+def decimal_to_snafu(x):
+    return "".join(reversed([_ for _ in gen_decimal_to_snafu(x)]))
+
+def calculate_part1(file: str):
+
+    s = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            s += snafu_to_decimal(line.rstrip())
+    return decimal_to_snafu(s)
+
+if __name__ == '__main__':
+    try:
+        file = sys.argv[1]
+    except:
+        file = "input.txt"
+
+    print("Dec 25, part 1: {}".format(calculate_part1(file)))
---
+++ 2022/dec25/test.py
@@ -0,0 +1,12 @@
+import unittest
+from solution import calculate_part1
+
+class TestDec25(unittest.TestCase):
+
+    file = "test.txt"
+
+    def test_part1(self):
+        self.assertEqual(calculate_part1(self.file), "2=-1=0")
+
+if __name__ == '__main__':
+    unittest.main()
