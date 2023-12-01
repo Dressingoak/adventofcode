36d1ac75dfd0ede452e21af5b397b7b584ed2fc5
Andreas Jessen <dressingoak@gmail.com>
2023-12-01 08:32:00 +0100

    Solution to Dec 1 2023, part 2

--- 2023/dec1/solution.py
+++ 2023/dec1/solution.py
@@ -1,4 +1,4 @@
-def calculate_part1(file: str):
+def part1(file: str):
     sum = 0
     with open(file, "r") as f:
         for line in f.readlines():
@@ -17,5 +17,54 @@ def calculate_part1(file: str):
     return sum
 
 
+def part2(file: str):
+    digits = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "1": 1, 
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6, 
+        "7": 7, 
+        "8": 8, 
+        "9": 9,
+    }
+
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            full = len(line)
+            first = True
+            last = None
+            loc = 0
+
+            while full - loc > 0:
+                broke = False
+                for pattern, value in digits.items():
+                    if line[loc:].startswith(pattern):
+                        if first:
+                            sum += value * 10
+                            first = False
+                        last = value
+                        loc += 1
+                        broke = True
+                        break
+                if not broke:
+                    loc += 1
+            sum += last
+    return sum
+
+
 if __name__ == '__main__':
-    print(f"Part 1: {calculate_part1('input.txt')}")
+    print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec1/test.py
+++ 2023/dec1/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import calculate_part1
+from solution import part1, part2
 
 class TestDec1(unittest.TestCase):
 
     def test_part1(self):
-        self.assertEqual(calculate_part1("test.txt"), 142)
+        self.assertEqual(part1("test.txt"), 142)
+
+    def test_part2(self):
+        self.assertEqual(part2("test2.txt"), 281)
 
 if __name__ == '__main__':
     unittest.main()
