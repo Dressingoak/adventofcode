daa9821b819482d5e8a7954b13f61024244a4d08
Andreas Jessen <dressingoak@gmail.com>
2023-12-02 23:26:26 +0100

    Solution to Dec 2 2023, part 2

--- 2023/dec2/solution.py
+++ 2023/dec2/solution.py
@@ -4,7 +4,7 @@ def part1(file: str):
         for line in f.readlines():
             allowed = True
             record, game = line.split(":")
-            _, num = record.split();
+            _, num = record.split()
             for pick in game.strip().split("; "):
                 for selection in pick.split(", "):
                     match selection.split(" "):
@@ -19,5 +19,27 @@ def part1(file: str):
     return sum
 
 
+def part2(file: str):
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            allowed = True
+            record, game = line.split(":")
+            _, num = record.split()
+            red, green, blue = 0, 0, 0
+            for pick in game.strip().split("; "):
+                for selection in pick.split(", "):
+                    match selection.split(" "):
+                        case [x, "red"]:
+                            red = max(int(x), red)
+                        case [x, "green"]:
+                            green = max(int(x), green)
+                        case [x, "blue"]:
+                            blue = max(int(x), blue)
+            sum += red * green * blue
+    return sum
+
+
 if __name__ == '__main__':
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec2/test.py
+++ 2023/dec2/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 class TestDec1(unittest.TestCase):
 
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 8)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 2286)
 
 if __name__ == '__main__':
     unittest.main()
