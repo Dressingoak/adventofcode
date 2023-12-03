3da79d6db5466fc3bade5c1484c85783b3105f05
Andreas Jessen <dressingoak@gmail.com>
2023-12-03 18:21:11 +0100

    Solution to Dec 3 2023, part 2

--- 2023/dec3/solution.py
+++ 2023/dec3/solution.py
@@ -1,5 +1,6 @@
-def part1(file: str):
-    sum = 0
+def read_input(
+    file: str,
+) -> (dict[tuple[int, int], str], list[int, list[tuple[int, int]]]):
     grid = {}
     digits = []
     with open(file, "r") as f:
@@ -12,18 +13,27 @@ def part1(file: str):
                     digit *= 10
                     digit += d
                     digit_len += 1
-                except:
+                except ValueError:
                     if digit > 0:
-                        digits.append((digit, [(i, k) for k in range(j - digit_len, j)]))
+                        digits.append(
+                            (digit, set([(i, k) for k in range(j - digit_len, j)]))
+                        )
                         digit = 0
                         digit_len = 0
                     match char:
-                        case '.' | '\n': pass
+                        case "." | "\n":
+                            pass
                         case _:
                             grid[(i, j)] = char
-    for (digit, positions) in digits:
+    return grid, digits
+
+
+def part1(file: str):
+    sum = 0
+    grid, digits = read_input(file)
+    for digit, positions in digits:
         b = False
-        for (i, j) in positions:
+        for i, j in positions:
             for ii in range(i - 1, i + 2):
                 if b:
                     break
@@ -37,5 +47,25 @@ def part1(file: str):
     return sum
 
 
-if __name__ == '__main__':
+def part2(file: str):
+    sum = 0
+    grid, digits = read_input(file)
+    outline = {}
+    for digit, positions in digits:
+        for i, j in positions:
+            for ii in range(i - 1, i + 2):
+                for jj in range(j - 1, j + 2):
+                    if (ii, jj) not in positions and (ii, jj) in grid:
+                        if (ii, jj) not in outline:
+                            outline[(ii, jj)] = set()
+                        outline[(ii, jj)].add((grid[(ii, jj)], digit))
+    for _, s in outline.items():
+        match list(s):
+            case [("*", l), ("*", r)]:
+                sum += l * r
+    return sum
+
+
+if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec3/test.py
+++ 2023/dec3/test.py
@@ -1,12 +1,14 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec1(unittest.TestCase):
-
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 4361)
 
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 467835)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
