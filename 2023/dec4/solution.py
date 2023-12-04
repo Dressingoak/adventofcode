ce0b20ac70139d786af742de44ad1b842c6b81ee
Andreas Jessen <dressingoak@gmail.com>
2023-12-04 07:39:43 +0100

    Solution to Dec 4 2023, part 2

--- 2023/dec4/solution.py
+++ 2023/dec4/solution.py
@@ -17,5 +17,28 @@ def part1(file: str):
     return sum
 
 
+def part2(file: str):
+    cards = {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            card, numbers = line.split(":")
+            _, num = card.split()
+            num = int(num)
+            if num not in cards:
+                cards[num] = 1
+            else:
+                cards[num] += 1
+            winning, actual = numbers.split("|")
+            winning = set([int(i) for i in winning.split()])
+            matches = sum(1 for _ in winning.intersection(int(i) for i in actual.split()))
+            for i in range(num + 1, num + matches + 1):
+                if i not in cards:
+                    cards[i] = cards[num]
+                else:
+                    cards[i] += cards[num]
+    return sum(v for v in cards.values())
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
