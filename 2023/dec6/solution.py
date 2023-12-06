e1011287f0fa8ecaac060b416af22c7144f1eb4d
Andreas Jessen <dressingoak@gmail.com>
2023-12-06 06:26:56 +0100

    Solution to Dec 6 2023, part 2
    Unoptimized part 2, takes ~10 s to finish

--- 2023/dec6/solution.py
+++ 2023/dec6/solution.py
@@ -17,5 +17,23 @@ def part1(file: str):
     return prod
 
 
+def part2(file: str):
+    prod = 1
+    with open(file, "r") as f:
+        for line in f.readlines():
+            match line.split(":"):
+                case ["Time", x]:
+                    time = int("".join(x.split()))
+                case ["Distance", x]:
+                    distance = int("".join(x.split()))
+    ways = 0
+    for t in range(1, time):
+        rem = time - t
+        if rem * t > distance:
+            ways += 1
+    return ways
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
