e0d9ff5ec98136c4f3db4582681e0b703d4d97b7
Andreas Jessen <dressingoak@gmail.com>
2023-12-07 18:40:09 +0100

    Generalized solution for Dec 7 2023

--- 2023/dec7/solution.py
+++ 2023/dec7/solution.py
@@ -1,28 +1,21 @@
-def get_value(hand: str, values: list[int]):
-    s = 0
-    for c in hand:
-        s *= 13
-        s += 13 - values.index(c)
-    return s
-
-
 def get_type_from_counts(sorted_counts: list[int]):
-    s = {}
     match sorted_counts:
         case [5]:
-            return 7  # Five of a kind
+            return 1  # Five of a kind
         case [1, 4]:
-            return 6  # Four of a kind
+            return 2  # Four of a kind
         case [2, 3]:
-            return 5  # Full house
+            return 3  # Full house
         case [1, 1, 3]:
             return 4  # Three of a kind
         case [1, 2, 2]:
-            return 3  # Two pairs
+            return 5  # Two pairs
         case [1, 1, 1, 2]:
-            return 2  # One pair
+            return 6  # One pair
         case [1, 1, 1, 1, 1]:
-            return 1  # High card
+            return 7  # High card
+        case _:
+            raise
 
 
 def get_type(hand: str):
@@ -35,8 +28,7 @@ def get_type(hand: str):
     return get_type_from_counts(sorted(_ for _ in s.values()))
 
 
-def part1(file: str):
-    values = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+def total_winnings(file: str, values: list[int], type_fun):
     max_rank = 0
     hands = []
     with open(file, "r") as f:
@@ -44,11 +36,21 @@ def part1(file: str):
             match line.split():
                 case [hand, bid]:
                     hands.append(
-                        (hand, int(bid), get_type(hand), get_value(hand, values))
+                        (
+                            hand,
+                            int(bid),
+                            type_fun(hand),
+                            *(values.index(c) for c in hand),
+                        )
                     )
             max_rank += 1
-    hands.sort(key=lambda x: (x[2], x[3]))
+    hands.sort(key=lambda x: tuple(x[2:]), reverse=True)
     return sum(r * hand[1] for r, hand in zip(range(1, max_rank + 1), hands))
+
+
+def part1(file: str):
+    values = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+    return total_winnings(file, values, get_type)
 
 
 def get_type_with_joker(hand: str):
@@ -66,30 +68,14 @@ def get_type_with_joker(hand: str):
                     vv if kk != k else vv + count for kk, vv in remaining.items()
                 )
                 types.append(get_type_from_counts(s_n))
-            return max(types, default=7)
+            return min(types, default=1)
         case _:
             return get_type_from_counts(sorted(_ for _ in s.values()))
 
 
 def part2(file: str):
     values = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
-    max_rank = 0
-    hands = []
-    with open(file, "r") as f:
-        for line in f.readlines():
-            match line.split():
-                case [hand, bid]:
-                    hands.append(
-                        (
-                            hand,
-                            int(bid),
-                            get_type_with_joker(hand),
-                            get_value(hand, values),
-                        )
-                    )
-            max_rank += 1
-    hands.sort(key=lambda x: (x[2], x[3]))
-    return sum(r * hand[1] for r, hand in zip(range(1, max_rank + 1), hands))
+    return total_winnings(file, values, get_type_with_joker)
 
 
 if __name__ == "__main__":
