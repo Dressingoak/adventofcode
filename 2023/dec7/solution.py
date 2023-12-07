ddbb485c8c3a64bb071b3131c4234e85d03783bd
Andreas Jessen <dressingoak@gmail.com>
2023-12-07 09:18:41 +0100

    Solution to Dec 7 2023, part 2

--- 2023/dec7/solution.py
+++ 2023/dec7/solution.py
@@ -51,5 +51,47 @@ def part1(file: str):
     return sum(r * hand[1] for r, hand in zip(range(1, max_rank + 1), hands))
 
 
+def get_type_with_joker(hand: str):
+    s = {}
+    for c in hand:
+        if c in s:
+            s[c] += 1
+        else:
+            s[c] = 1
+    match s:
+        case {"J": count, **remaining}:
+            types = []
+            for k in remaining.keys():
+                s_n = sorted(
+                    vv if kk != k else vv + count for kk, vv in remaining.items()
+                )
+                types.append(get_type_from_counts(s_n))
+            return max(types, default=7)
+        case _:
+            return get_type_from_counts(sorted(_ for _ in s.values()))
+
+
+def part2(file: str):
+    values = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+    max_rank = 0
+    hands = []
+    with open(file, "r") as f:
+        for line in f.readlines():
+            match line.split():
+                case [hand, bid]:
+                    hands.append(
+                        (
+                            hand,
+                            int(bid),
+                            get_type_with_joker(hand),
+                            get_value(hand, values),
+                        )
+                    )
+            max_rank += 1
+    hands.sort(key=lambda x: (x[2], x[3]))
+    return sum(r * hand[1] for r, hand in zip(range(1, max_rank + 1), hands))
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
