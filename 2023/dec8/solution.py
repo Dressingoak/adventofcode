12cd861929ee8a9d191f7dec619c92f7ce5df689
Andreas Jessen <dressingoak@gmail.com>
2023-12-08 08:05:31 +0100

    Solution to Dec 8 2023, part 2

--- 2023/dec8/solution.py
+++ 2023/dec8/solution.py
@@ -1,4 +1,4 @@
-def part1(file: str):
+def parse(file: str):
     connections = {}
     with open(file, "r") as f:
         for line in f.readlines():
@@ -7,7 +7,11 @@ def part1(file: str):
                     instructions = [0 if c == "L" else 1 for c in ins]
                 case [start, _, l, r]:
                     connections[start] = (l[1:4], r[:3])
+    return instructions, connections
 
+
+def part1(file: str):
+    instructions, connections = parse(file)
     instructions_total = len(instructions)
     i = 0
     node = "AAA"
@@ -18,5 +22,56 @@ def part1(file: str):
     return i
 
 
+def gcd(a, b):
+    while b != 0:
+        t = b
+        b = a % b
+        a = t
+    return a
+
+
+def part2(file: str):
+    instructions, connections = parse(file)
+    instructions_total = len(instructions)
+    starts = [node for node in connections.keys() if node.endswith("A")]
+    ends = {}
+    for start in starts:
+        end = {}
+        i = 0
+        node = start
+        explored = False
+        while not explored:
+            node = connections[node][instructions[i % instructions_total]]
+            i += 1
+            if node == start and i % instructions_total == 0:
+                break
+            if node.endswith("Z"):
+                if node not in end:
+                    end[node] = (i, [])
+                else:
+                    first = end[node][0]
+                    if (i - first) % instructions_total == first % instructions_total:
+                        explored = True
+                    else:
+                        for seen in end[node][1]:
+                            if (
+                                i - seen
+                            ) % instructions_total == first % instructions_total:
+                                explored = True
+                                break
+                    if not explored:
+                        end[node][1].append(i)
+        ends[start] = end
+    cycle = 1
+    for node, reps in ends.items():
+        match list(reps.values()):
+            case [(value, [])]:
+                cycle = (cycle * value) // gcd(cycle, value)
+            case _:
+                raise Exception("Shifted cycles not handled")
+    return cycle
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec8/test.py
+++ 2023/dec8/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec8(unittest.TestCase):
@@ -8,6 +8,9 @@ class TestDec8(unittest.TestCase):
 
     def test_part1_2(self):
         self.assertEqual(part1("test2.txt"), 6)
+
+    def test_part2(self):
+        self.assertEqual(part2("test3.txt"), 6)
 
 
 if __name__ == "__main__":
