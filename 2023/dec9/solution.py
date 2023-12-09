ffaaea39757c78c55679802ba3c8e9fa9d25050e
Andreas Jessen <dressingoak@gmail.com>
2023-12-09 10:05:14 +0100

    Solution to Dec 9 2023, part 2

--- 2023/dec9/solution.py
+++ 2023/dec9/solution.py
@@ -1,18 +1,35 @@
-def predict(values: list[int]):
+def forward(values: list[int]):
     diff = [values[i + 1] - values[i] for i in range(len(values) - 1)]
     if all(x == 0 for x in diff):
         return values[-1]
     else:
-        return values[-1] + predict(diff)
+        return values[-1] + forward(diff)
 
 
 def part1(file: str):
     sum = 0
     with open(file, "r") as f:
         for line in f.readlines():
-            sum += predict([int(_) for _ in line.split(" ")])
+            sum += forward([int(_) for _ in line.split(" ")])
+    return sum
+
+
+def backward(values: list[int]):
+    diff = [values[i + 1] - values[i] for i in range(len(values) - 1)]
+    if all(x == 0 for x in diff):
+        return values[0]
+    else:
+        return values[0] - backward(diff)
+
+
+def part2(file: str):
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            sum += backward([int(_) for _ in line.split(" ")])
     return sum
 
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec9/test.py
+++ 2023/dec9/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec9(unittest.TestCase):
     def test_part1_1(self):
         self.assertEqual(part1("test.txt"), 114)
+
+    def test_part1_1(self):
+        self.assertEqual(part2("test.txt"), 114)
 
 
 if __name__ == "__main__":
