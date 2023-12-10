db5bc74099e8b82cc2b10e61f7149f187d932b52
Andreas Jessen <dressingoak@gmail.com>
2023-12-10 23:09:35 +0100

    Solution to Dec 10 2023, part 2

--- 2023/dec10/solution.py
+++ 2023/dec10/solution.py
@@ -19,9 +19,9 @@ def follow_main_loop(file: str):
                 row.append(c)
                 if c == "S":
                     start = (i, j)
-                    yield start
             grid.append(row)
     rows, cols = len(grid), len(grid[0])
+    yield rows, cols
     i, j = start
     adjacent = {
         k: v
@@ -32,16 +32,21 @@ def follow_main_loop(file: str):
         }.items()
         if v != "." and (k + 2) % 4 in pieces[v]
     }
-    d = next(_ for _ in adjacent.keys())
+    sd1, sd2 = tuple(adjacent.keys())
+    for piece, (d1, d2) in pieces.items():
+        if (sd1 == d1 and sd2 == d2) or (sd1 == d2 and sd2 == d1):
+            d = d1
+            break
+    yield piece, start
     di, dj = directions[d]
     node = (i + di, j + dj)
     prev_dir = (d + 2) % 4
     while node != start:
-        yield node
         k, l = node
         piece = grid[k][l]
         match next(d for d in pieces[piece] if d != prev_dir):
             case d:
+                yield piece, node
                 (dk, dl) = directions[d]
                 node = (k + dk, l + dl)
                 prev_dir = (d + 2) % 4
@@ -49,13 +54,41 @@ def follow_main_loop(file: str):
 
 def part1(file: str):
     d = 0
-    for _ in follow_main_loop(file):
+    g = follow_main_loop(file)
+    next(g)
+    for _ in g:
         d += 1
     return d // 2
 
 
 def part2(file: str):
-    return 0
+    g = follow_main_loop(file)
+    rows, cols = next(g)
+    grid = {}
+    for i in range(rows):
+        grid[i] = []
+    for piece, (i, j) in g:
+        grid[i].append((j, piece))
+    count = 0
+    for i in range(rows):
+        grid[i].sort()
+        inside = False
+        j_prev = None
+        carry = None
+        for j, piece in grid[i]:
+            if inside and piece != "-" and j_prev is not None and j - j_prev > 0:
+                count += j - (j_prev + 1)
+            match piece:
+                case "F" | "L":
+                    carry = piece
+                case "|":
+                    inside = not inside
+                case "J" if carry == "F":
+                    inside = not inside
+                case "7" if carry == "L":
+                    inside = not inside
+            j_prev = j
+    return count
 
 
 if __name__ == "__main__":
--- 2023/dec10/test.py
+++ 2023/dec10/test.py
@@ -8,15 +8,18 @@ class TestDec9(unittest.TestCase):
 
     def test_part1_2(self):
         self.assertEqual(part1("test2.txt"), 8)
-    
+
     def test_part2_1(self):
-        self.assertEqual(part2("test3.txt"), 4)
+        self.assertEqual(part2("test3a.txt"), 4)
 
     def test_part2_2(self):
-        self.assertEqual(part2("test4.txt"), 8)
-    
+        self.assertEqual(part2("test3b.txt"), 4)
+
     def test_part2_3(self):
-        self.assertEqual(part2("test4.txt"), 10)
+        self.assertEqual(part2("test4.txt"), 8)
+
+    def test_part2_3(self):
+        self.assertEqual(part2("test5.txt"), 10)
 
 
 if __name__ == "__main__":
