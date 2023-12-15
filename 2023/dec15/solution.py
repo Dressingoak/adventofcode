913ead24e5353fad8d7dfe462826010b4ba38420
Andreas Jessen <dressingoak@gmail.com>
2023-12-15 07:00:38 +0100

    Solution to Dec 15 2023, part 2

--- 2023/dec15/solution.py
+++ 2023/dec15/solution.py
@@ -1,16 +1,66 @@
+def HASH(seq: str) -> int:
+    current_value = 0
+    for char in seq:
+        current_value += ord(char)
+        current_value *= 17
+        current_value %= 256
+    return current_value
+
+
 def part1(file: str):
     sum = 0
     with open(file, "r") as f:
         for line in f.readlines():
             for seq in line.strip().split(","):
-                current_value = 0
-                for char in seq:
-                    current_value += ord(char)
-                    current_value *= 17
-                    current_value %= 256
-                sum += current_value
+                sum += HASH(seq)
+    return sum
+
+
+def part2(file: str):
+    sum = 0
+    boxes = {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            for seq in line.strip().split(","):
+                if seq.endswith("-"):
+                    label = seq[:-1]
+                    h = HASH(label)
+                    if h in boxes:
+                        boxes[h]
+                        i = -1
+                        for j, (item, _) in enumerate(boxes[h]):
+                            if label == item:
+                                i = j
+                                break
+                        if i != -1:
+                            boxes[h].pop(i)
+                        if len(boxes[h]) == 0:
+                            del boxes[h]
+                else:
+                    label, value = seq.split("=")
+                    value = int(value)
+                    h = HASH(label)
+                    if h not in boxes:
+                        boxes[h] = [(label, value)]
+                    else:
+                        i = -1
+                        for j, (item, _) in enumerate(boxes[h]):
+                            if label == item:
+                                i = j
+                                break
+                        if i != -1:
+                            boxes[h][i] = (label, value)
+                        else:
+                            boxes[h].append((label, value))
+    sum = 0
+    for box, lists in boxes.items():
+        v1 = box + 1
+        for i, (_, v3) in enumerate(lists):
+            v2 = i + 1
+            sum += v1 * v2 * v3
     return sum
 
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec15/test.py
+++ 2023/dec15/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec15(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 1320)
+
+    def test_part1(self):
+        self.assertEqual(part2("test.txt"), 145)
 
 
 if __name__ == "__main__":
