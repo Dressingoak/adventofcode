eb2fd346275c3162a97299df14745541b5dfd634
Andreas Jessen <dressingoak@gmail.com>
2023-12-19 09:36:44 +0100

    Solution to Dec 19 2023, part 2

--- 2023/dec19/solution.py
+++ 2023/dec19/solution.py
@@ -42,5 +42,66 @@ def part1(file: str):
     return total
 
 
+def evaluate(workflows, goto, part, i):
+    if goto == "A":
+        ways = 1
+        for v in part.values():
+            ways *= v[1] - v[0]
+        yield ways
+    elif goto == "R":
+        pass
+    else:
+        match workflows[goto][i]:
+            case (cat, "<", value, nxt):
+                if part[cat][1] <= value:
+                    yield (nxt, part, 0)
+                elif part[cat][0] > value:
+                    yield (goto, part, i + 1)
+                else:
+                    yield (goto, {**part, cat: (value, part[cat][1])}, i + 1)
+                    yield (nxt, {**part, cat: (part[cat][0], value)}, 0)
+            case (cat, ">", value, nxt):
+                if part[cat][0] > value:
+                    yield (nxt, part, 0)
+                elif part[cat][1] <= value:
+                    yield (goto, part, i + 1)
+                else:
+                    yield (nxt, {**part, cat: (value + 1, part[cat][1])}, 0)
+                    yield (goto, {**part, cat: (part[cat][0], value + 1)}, i + 1)
+            case nxt:
+                yield (nxt, part, 0)
+
+
+def part2(file: str):
+    total = 0
+    workflows = {}
+    with open(file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                break
+            name, wf = line.split("{")
+            rules = wf[:-1].split(",")
+            parsed = []
+            for section in rules:
+                match section.split(":"):
+                    case [r, dst]:
+                        parsed.append((r[0], r[1], int(r[2:]), dst))
+                    case [dst]:
+                        parsed.append(dst)
+            workflows[name] = parsed
+    stack = [("in", {k: (1, 4001) for k in ["x", "m", "a", "s"]}, 0)]
+    while len(stack) > 0:
+        goto, part, i = stack.pop()
+        for ret in evaluate(workflows, goto, part, i):
+            match ret:
+                case s if isinstance(s, int):
+                    total += s
+                case triple:
+                    stack.append(triple)
+    return total
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2023/dec19/test.py
+++ 2023/dec19/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec19(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 19114)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 167409079868000)
 
 
 if __name__ == "__main__":
