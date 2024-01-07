b7d10f4eaf1fc7f0b0accbe7523ec12909e8b3e2
Andreas Jessen <dressingoak@gmail.com>
2024-01-07 20:57:43 +0100

    Solution to Dec 24 2023 without imports

--- 2023/dec24/solution.py
+++ 2023/dec24/solution.py
@@ -51,13 +51,118 @@ def part1(file: str, least=200000000000000, most=400000000000000):
     return count
 
 
+def gcd(a, b):
+    while b != 0:
+        t = b
+        b = a % b
+        a = t
+    return a
+
+
+class Fraction:
+    def __init__(self, a, b=1):
+        if b < 0:
+            div = gcd(-a, -b)
+            self.a, self.b = -a // div, -b // div
+        else:
+            div = gcd(a, b)
+            self.a, self.b = a // div, b // div
+
+    def __neg__(self):
+        return Fraction(-self.a, self.b)
+
+    def __int__(self):
+        return self.a // self.b
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            c, d = other, 1
+        else:
+            c, d = other.a, other.b
+        return Fraction(self.a * d + c * self.b, self.b * d)
+
+    def __sub__(self, other):
+        return self.__add__(-other)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            c, d = other, 1
+        else:
+            c, d = other.a, other.b
+        return Fraction(self.a * c, self.b * d)
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            c, d = other, 1
+        else:
+            c, d = other.a, other.b
+        return Fraction(self.a * d, self.b * c)
+
+    def __rtruediv__(self, other):
+        return self / other
+
+    def __abs__(self):
+        return Fraction(-self.a if self.a < 0 else self.a, self.b)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, int):
+            c, d = other, 1
+        else:
+            c, d = other.a, other.b
+        return self.a == c and self.b == d
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            c, d = other, 1
+        else:
+            c, d = other.a, other.b
+        return self.a * d < c * self.b
+
+    def __repr__(self):
+        if self.b == 1:
+            return f"{self.a}"
+        else:
+            return f"{self.a}/{self.b}"
+
+
+def gauss_jordan_elimination(A):
+    n, m = len(A), len(A[0])
+    h, k = 0, 0
+    while h < n and k < m:
+        i_max = max((i for i in range(h, n)), key=lambda i: abs(A[i][k]))  # argmax
+        if A[i_max][k] == 0:
+            k += 1
+        else:
+            A[i_max], A[h] = A[h], A[i_max]  # swap rows
+            for i in range(h + 1, n):
+                f = A[i][k] / A[h][k]
+                A[i][k] = Fraction(0)
+                for j in range(k + 1, m):
+                    A[i][j] -= A[h][j] * f
+            h += 1
+            k += 1
+    if any(A[h][h] == 0 for h in range(n)):
+        raise ValueError("Non-invertible matrix")
+    for h in reversed(range(0, n)):
+        for j in reversed(range(h, m)):
+            A[h][j] /= A[h][h]
+        for i in reversed(range(0, h)):
+            f = A[i][h]
+            A[i][h] = Fraction(0)
+            for j in range(n, m):
+                A[i][j] -= A[h][j] * f
+    return A
+
+
+def solve_linear_system(A, c):
+    argmented = []
+    for i in range(len(A)):
+        argmented.append([Fraction(_) for _ in A[i]] + [Fraction(_) for _ in c[i]])
+    gauss_jordan_elimination(argmented)
+    return [argmented[i][len(A) :] for i in range(len(A))]
+
+
 def part2(file: str):
-    from sympy import solve, symbols
-
-    t1, t2, t3 = symbols("t1 t2 t3")
-    p1, p2, p3 = symbols("p1 p2 p3")
-    v1, v2, v3 = symbols("v1 v2 v3")
-
     hailstorms = []
     with open(file) as f:
         for line in f:
@@ -66,17 +171,30 @@ def part2(file: str):
                 ([int(_) for _ in p.split(", ")], [int(_) for _ in v.split(", ")])
             )
 
-    equations = []
-    for i in range(3):
-        for d in range(3):
-            equations.append(
-                hailstorms[i][0][d]
-                + eval(f"t{i+1}") * hailstorms[i][1][d]
-                - eval(f"p{d+1}")
-                - eval(f"t{i+1}") * eval(f"v{d+1}")
+    A, c = [], []  # A * x = c
+    for i, j in [(0, 1), (0, 2)]:
+        for d1, d2 in [[0, 1], [0, 2], [1, 2]]:  # (x,y), (x,z), (y,z)
+            c.append(
+                [
+                    hailstorms[j][0][d1] * hailstorms[j][1][d2]
+                    - hailstorms[j][0][d2] * hailstorms[j][1][d1]
+                    - (
+                        hailstorms[i][0][d1] * hailstorms[i][1][d2]
+                        - hailstorms[i][0][d2] * hailstorms[i][1][d1]
+                    )
+                ]
             )
-    res = solve(equations, t1, t2, t3, p1, p2, p3, v1, v2, v3, dict=True)[0]
-    return res[p1] + res[p2] + res[p3]
+            row = [0] * 6
+            row[d1] = -(hailstorms[i][1][d2] - hailstorms[j][1][d2])  # p_d1
+            row[d2] = hailstorms[i][1][d1] - hailstorms[j][1][d1]  # p_d2
+            row[d1 + 3] = hailstorms[i][0][d2] - hailstorms[j][0][d2]  # v_d1
+            row[d2 + 3] = -(hailstorms[i][0][d1] - hailstorms[j][0][d1])  # v_d2
+            A.append(row)
+
+    x = solve_linear_system(A, c)
+
+    p1, p2, p3 = x[0][0], x[1][0], x[2][0]
+    return int(p1 + p2 + p3)
 
 
 if __name__ == "__main__":
--- 2023/dec24/test.py
+++ 2023/dec24/test.py
@@ -1,10 +1,40 @@
 import unittest
-from solution import part1, part2
+from solution import part1, part2, Fraction, solve_linear_system
 
 
 class TestDec23(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt", least=7, most=27), 2)
+
+    def test_gauss_jordan_matrix_inversion(self):
+        matrix = [
+            [2, -1, 0],
+            [-1, 2, -1],
+            [0, -1, 2],
+        ]
+        identity = [[1 if i == j else 0 for j in range(3)] for i in range(3)]
+        res = solve_linear_system(matrix, identity)
+        expected = [
+            [Fraction(3, 4), Fraction(1, 2), Fraction(1, 4)],
+            [Fraction(1, 2), Fraction(1), Fraction(1, 2)],
+            [Fraction(1, 4), Fraction(1, 2), Fraction(3, 4)],
+        ]
+        self.assertEqual(res, expected)
+
+    def test_gauss_jordan_linear_system(self):
+        A = [
+            [2, 1, -1],
+            [-3, -1, 2],
+            [-2, 1, 2],
+        ]
+        c = [[8], [-11], [-3]]
+        x = solve_linear_system(A, c)
+        expected = [
+            [Fraction(2)],
+            [Fraction(3)],
+            [Fraction(-1)],
+        ]
+        self.assertEqual(x, expected)
 
     def test_part2(self):
         self.assertEqual(part2("test.txt"), 47)
