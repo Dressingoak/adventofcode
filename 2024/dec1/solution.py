cf20c0cf3b57920fb3a2bdbcb7db8d4564bc7204
Andreas Jessen <dressingoak@gmail.com>
2024-12-01 06:37:35 +0100

    Solution to Dec 1 2024, part 2

--- 2024/dec1/solution.py
+++ 2024/dec1/solution.py
@@ -13,5 +13,23 @@ def part1(file: str):
     return sum
 
 
+def part2(file: str):
+    sum = 0
+    l1, l2 = [], {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            e1, e2 = line.split()
+            l1.append(int(e1))
+            e2 = int(e2)
+            if e2 in l2:
+                l2[e2] += 1
+            else:
+                l2[e2] = 1
+    for e1 in l1:
+        sum += e1 * l2.get(e1, 0)
+    return sum
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec1/test.py
+++ 2024/dec1/test.py
@@ -1,10 +1,15 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec1(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 11)
+
+
+class TestDec2(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(part2("test.txt"), 11)
 
 
 if __name__ == "__main__":
