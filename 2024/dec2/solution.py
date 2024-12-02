491a296bf6e40600d9e46211bab7976ba0f73771
Andreas Jessen <dressingoak@gmail.com>
2024-12-02 07:52:23 +0100

    Solution to Dec 2 2024, part 2

--- 2024/dec2/solution.py
+++ 2024/dec2/solution.py
@@ -1,23 +1,37 @@
+def report_safety(levels):
+    diffs = [levels[i + 1] - levels[i] for i in range(len(levels) - 1)]
+    return all(d >= 1 and d <= 3 for d in diffs) or all(
+        d <= -1 and d >= -3 for d in diffs
+    )
+
+
 def part1(file: str):
     safe = 0
     with open(file, "r") as f:
         for report in f.readlines():
             levels = [int(_) for _ in report.split()]
-            sign = 0
-            invalid = False
-            for i in range(0, len(levels) - 1):
-                diff = levels[i+1] - levels[i]
-                if diff > 0 and sign >= 0 and diff <= 3:
-                    sign = 1
-                elif diff < 0 and sign <= 0 and diff >= -3:
-                    sign = -1
-                else:
-                    invalid = True
+            safe += int(report_safety(levels))
+    return safe
+
+
+def iter_levels_tolerable(levels):
+    yield (-1, levels)
+    for i in range(len(levels)):
+        yield (i, levels[:i] + levels[(i + 1) :])
+
+
+def part2(file: str):
+    safe = 0
+    with open(file, "r") as f:
+        for report in f.readlines():
+            levels = [int(_) for _ in report.split()]
+            for i, l in iter_levels_tolerable(levels):
+                if report_safety(l):
+                    safe += 1
                     break
-            if not invalid:
-                safe += 1
     return safe
 
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec2/test.py
+++ 2024/dec2/test.py
@@ -1,11 +1,15 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec1(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 2)
 
+
+class TestDec2(unittest.TestCase):
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 4)
 
 if __name__ == "__main__":
     unittest.main()
