3f4a8398a36c5578353ae8c5b9d61a93a2a2c3ce
Andreas Jessen <dressingoak@gmail.com>
2024-12-02 08:24:28 +0100

    2x speedup by using first report checker

--- 2024/dec2/solution.py
+++ 2024/dec2/solution.py
@@ -1,8 +1,14 @@
 def report_safety(levels):
-    diffs = [levels[i + 1] - levels[i] for i in range(len(levels) - 1)]
-    return all(d >= 1 and d <= 3 for d in diffs) or all(
-        d <= -1 and d >= -3 for d in diffs
-    )
+    sign = 0
+    for i in range(0, len(levels) - 1):
+        diff = levels[i + 1] - levels[i]
+        if diff > 0 and sign >= 0 and diff <= 3:
+            sign = 1
+        elif diff < 0 and sign <= 0 and diff >= -3:
+            sign = -1
+        else:
+            return False
+    return True
 
 
 def part1(file: str):
@@ -15,9 +21,9 @@ def part1(file: str):
 
 
 def iter_levels_tolerable(levels):
-    yield (-1, levels)
+    yield levels
     for i in range(len(levels)):
-        yield (i, levels[:i] + levels[(i + 1) :])
+        yield levels[:i] + levels[(i + 1) :]
 
 
 def part2(file: str):
@@ -25,7 +31,7 @@ def part2(file: str):
     with open(file, "r") as f:
         for report in f.readlines():
             levels = [int(_) for _ in report.split()]
-            for i, l in iter_levels_tolerable(levels):
+            for l in iter_levels_tolerable(levels):
                 if report_safety(l):
                     safe += 1
                     break
