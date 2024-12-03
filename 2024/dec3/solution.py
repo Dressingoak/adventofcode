51ee3165bfae63354963db0e5becbb901e1e9e3f
Andreas Jessen <dressingoak@gmail.com>
2024-12-03 06:21:02 +0100

    Solution to Dec 3 2024, part 2

--- 2024/dec3/solution.py
+++ 2024/dec3/solution.py
@@ -1,5 +1,6 @@
 import re
 
+
 def part1(file: str):
     sum = 0
     pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
@@ -9,5 +10,23 @@ def part1(file: str):
                 sum += int(match.group(1)) * int(match.group(2))
     return sum
 
+
+def part2(file: str):
+    sum = 0
+    pattern = re.compile(r"don't\(\)|do\(\)|mul\((\d{1,3}),(\d{1,3})\)")
+    enabled = True
+    with open(file, "r") as f:
+        for line in f.readlines():
+            for match in pattern.finditer(line):
+                if match.group(0) == "do()":
+                    enabled = True
+                elif match.group(0) == "don't()":
+                    enabled = False
+                elif enabled:
+                    sum += int(match.group(1)) * int(match.group(2))
+    return sum
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec3/test.py
+++ 2024/dec3/test.py
@@ -1,10 +1,16 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec1(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 161)
+
+
+class TestDec2(unittest.TestCase):
+    def test_part2(self):
+        self.assertEqual(part2("test2.txt"), 48)
+
 
 if __name__ == "__main__":
     unittest.main()
