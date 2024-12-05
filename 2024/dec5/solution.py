a141983f68233e7a8a622637e74ad7b1c2548140
Andreas Jessen <dressingoak@gmail.com>
2024-12-05 07:00:06 +0100

    Solution to Dec 5 2024, part 2

--- 2024/dec5/solution.py
+++ 2024/dec5/solution.py
@@ -27,9 +27,46 @@ def part1(file: str):
                         break
                 if acceptable:
                     sum += pages[len(pages) // 2]
+    return sum
 
+
+def part2(file: str):
+    sum = 0
+    parse_orderings = True
+    page_orders = {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line == "":
+                parse_orderings = False
+                continue
+            if parse_orderings:
+                page, before = line.split("|")
+                page, before = int(page), int(before)
+                if page in page_orders:
+                    page_orders[page].add(before)
+                else:
+                    page_orders[page] = set([before])
+            else:
+                pages = [int(_) for _ in line.split(",")]
+                n = len(pages)
+                acceptable = False
+                corrected = False
+                while not acceptable:
+                    for i in range(n - 1):
+                        a, b = pages[i], pages[i + 1]
+                        if a in page_orders and b in page_orders[a]:
+                            if i + 2 == n:
+                                acceptable = True
+                        else:
+                            pages[i + 1], pages[i] = pages[i], pages[i + 1]
+                            corrected = True
+                            break
+                if corrected:
+                    sum += pages[len(pages) // 2]
     return sum
 
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec5/test.py
+++ 2024/dec5/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec5(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 143)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 123)
 
 
 if __name__ == "__main__":
