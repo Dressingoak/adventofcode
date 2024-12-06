c11cce0fd2fa2d3cc8925729506493582669a667
Andreas Jessen <dressingoak@gmail.com>
2024-12-06 20:17:45 +0100

    Slow solution to Dec 6 2024, part 2

--- 2024/dec6/solution.py
+++ 2024/dec6/solution.py
@@ -1,5 +1,4 @@
 def part1(file: str):
-    sum = 0
     map = []
     with open(file, "r") as f:
         for i, line in enumerate(f.readlines()):
@@ -19,7 +18,7 @@ def part1(file: str):
     while active:
         try:
             match dir:
-                case 0:  # left
+                case 0:  # right
                     for j in range(j, cols):
                         if map[i][j + 1] == "#":
                             dir = 3
@@ -31,7 +30,7 @@ def part1(file: str):
                             dir = 0
                             break
                         visited.add((i, j))
-                case 2:  # right
+                case 2:  # left
                     for j in range(j, -1, -1):
                         if map[i][j - 1] == "#":
                             dir = 1
@@ -49,5 +48,90 @@ def part1(file: str):
     return len(visited)
 
 
+def part2(file: str):
+    map_orig = []
+    with open(file, "r") as f:
+        for i, line in enumerate(f.readlines()):
+            row = []
+            for j, e in enumerate(line.strip()):
+                if e == "^":
+                    row.append(".")
+                    pos = (i, j)
+                    dir_orig = 1  # up
+                else:
+                    row.append(e)
+            map_orig.append(row)
+    i, j = pos
+    rows, cols = len(map_orig), len(map_orig[0])
+    ways = 0
+    for k in range(rows):
+        for l in range(cols):
+            if (k, l) == pos:
+                continue
+            i, j = pos
+            dir = dir_orig
+            map = [[_ for _ in map_orig[r]] for r in range(rows)]
+            map[k][l] = "#"
+            visited = set()
+            loop = False
+            active = True
+            while active and not loop:
+                if i == 6 and j == 3:
+                    pass
+                try:
+                    match dir:
+                        case 0:  # right
+                            for j in range(j, cols):
+                                if map[i][j + 1] == "#":
+                                    dir = 3
+                                    break
+                                if (i, j, dir) not in visited:
+                                    visited.add((i, j, dir))
+                                else:
+                                    loop = True
+                                    break
+                        case 1:  # up
+                            for i in range(i, -1, -1):
+                                if i == 0:
+                                    raise IndexError
+                                if map[i - 1][j] == "#":
+                                    dir = 0
+                                    break
+                                if (i, j, dir) not in visited:
+                                    visited.add((i, j, dir))
+                                else:
+                                    loop = True
+                                    break
+                        case 2:  # left
+                            for j in range(j, -1, -1):
+                                if j == 0:
+                                    raise IndexError
+                                if map[i][j - 1] == "#":
+                                    dir = 1
+                                    break
+                                if (i, j, dir) not in visited:
+                                    visited.add((i, j, dir))
+                                else:
+                                    loop = True
+                                    break
+                        case 3:  # down
+                            for i in range(i, rows):
+                                if map[i + 1][j] == "#":
+                                    dir = 2
+                                    break
+                                if (i, j, dir) not in visited:
+                                    visited.add((i, j, dir))
+                                else:
+                                    loop = True
+                                    break
+                except IndexError:
+                    visited.add((i, j, dir))
+                    active = False
+            if loop:
+                ways += 1
+    return ways
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec6/test.py
+++ 2024/dec6/test.py
@@ -1,10 +1,14 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec6(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 41)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 6)
+
 
 if __name__ == "__main__":
     unittest.main()
