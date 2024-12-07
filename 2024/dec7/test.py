8446ff900e062bd6b5f56e4f6d51a4610104ddae
Andreas Jessen <dressingoak@gmail.com>
2024-12-07 06:35:47 +0100

    Solution to Dec 7 2024, part 2

--- 2024/dec7/test.py
+++ 2024/dec7/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec7(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 3749)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 11387)
 
 
 if __name__ == "__main__":
