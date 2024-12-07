f7e6f57321aa9c2be27388910291de04a64cfbea
Andreas Jessen <dressingoak@gmail.com>
2024-12-07 06:41:37 +0100

    Condensed solution a bit

--- 2024/dec7/solution.py
+++ 2024/dec7/solution.py
@@ -1,54 +1,38 @@
-def iter(numbers):
+def iter(numbers: list[int], concat: bool):
+    """Applies operators right-to-left"""
     if len(numbers) == 1:
         yield numbers[0]
     else:
-        for res in iter(numbers[1:]):
+        for res in iter(numbers[1:], concat):
             yield numbers[0] + res
             yield numbers[0] * res
+            if concat:
+                yield int(str(res) + str(numbers[0]))
+
+
+def solve(file: str, concat: bool):
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            parts = line.split(" ")
+            result = int(parts[0][:-1])
+            numbers = [int(_) for _ in parts[-1 : -len(parts) : -1]]
+            possible = False
+            for x in iter(numbers, concat):
+                if x == result:
+                    possible = True
+                    break
+            if possible:
+                sum += result
+    return sum
 
 
 def part1(file: str):
-    sum = 0
-    with open(file, "r") as f:
-        for line in f.readlines():
-            parts = line.split(" ")
-            result = int(parts[0][:-1])
-            numbers = [int(_) for _ in parts[-1 : -len(parts) : -1]]
-            possible = False
-            for x in iter(numbers):
-                if x == result:
-                    possible = True
-                    break
-            if possible:
-                sum += result
-    return sum
-
-
-def iter_concat(numbers):
-    if len(numbers) == 1:
-        yield numbers[0]
-    else:
-        for res in iter_concat(numbers[1:]):
-            yield numbers[0] + res
-            yield numbers[0] * res
-            yield int(str(res) + str(numbers[0]))
+    return solve(file, False)
 
 
 def part2(file: str):
-    sum = 0
-    with open(file, "r") as f:
-        for line in f.readlines():
-            parts = line.split(" ")
-            result = int(parts[0][:-1])
-            numbers = [int(_) for _ in parts[-1 : -len(parts) : -1]]
-            possible = False
-            for x in iter_concat(numbers):
-                if x == result:
-                    possible = True
-                    break
-            if possible:
-                sum += result
-    return sum
+    return solve(file, True)
 
 
 if __name__ == "__main__":
