34432370c85fd43160ad7a2fb53f4494682a09cd
Andreas Jessen <dressingoak@gmail.com>
2024-12-07 06:26:54 +0100

    Solution to Dec 7 2024, part 1

---
+++ 2024/dec7/solution.py
@@ -0,0 +1,28 @@
+def iter(numbers):
+    if len(numbers) == 1:
+        yield numbers[0]
+    else:
+        for res in iter(numbers[1:]):
+            yield numbers[0] + res
+            yield numbers[0] * res
+
+
+def part1(file: str):
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            parts = line.split(" ")
+            result = int(parts[0][:-1])
+            numbers = [int(_) for _ in parts[-1 : -len(parts) : -1]]
+            possible = False
+            for x in iter(numbers):
+                if x == result:
+                    possible = True
+                    break
+            if possible:
+                sum += result
+    return sum
+
+
+if __name__ == "__main__":
+    print(f"{part1('input.txt')=}")
