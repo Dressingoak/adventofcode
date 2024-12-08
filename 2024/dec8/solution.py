19eac4aa822b15104e4d6a6ca350dd832316f8ce
Andreas Jessen <dressingoak@gmail.com>
2024-12-08 06:58:31 +0100

    Solution to Dec 8 2024, part 2

--- 2024/dec8/solution.py
+++ 2024/dec8/solution.py
@@ -33,5 +33,39 @@ def part1(file: str):
     return sum
 
 
+def part2(file: str):
+    sum = 0
+    antennas = {}
+    with open(file, "r") as f:
+        for i, line in enumerate(f.readlines()):
+            for j, e in enumerate(line.strip()):
+                if e != ".":
+                    if e in antennas:
+                        antennas[e].append((i, j))
+                    else:
+                        antennas[e] = [(i, j)]
+    rows, cols = i + 1, j + 1
+    found = set()
+    for antennas in antennas.values():
+        for il in range(len(antennas) - 1):
+            for ir in range(il + 1, len(antennas)):
+                l, r = antennas[il], antennas[ir]
+                diff = (r[0] - l[0], r[1] - l[1])
+                for start, m in [(l, -1), (r, 1)]:
+                    anti = start
+                    while (
+                        0 <= anti[0]
+                        and anti[0] < rows
+                        and 0 <= anti[1]
+                        and anti[1] < cols
+                    ):
+                        if anti not in found:
+                            sum += 1
+                            found.add(anti)
+                        anti = (anti[0] + m * diff[0], anti[1] + m * diff[1])
+    return sum
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
