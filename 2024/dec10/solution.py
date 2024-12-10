3b3cbdf1ab9d825b5330b77186f891069aa1dc37
Andreas Jessen <dressingoak@gmail.com>
2024-12-10 06:51:47 +0100

    Solution to Dec 10 2024, part 2

--- 2024/dec10/solution.py
+++ 2024/dec10/solution.py
@@ -5,8 +5,8 @@ def part1(file: str):
         for rows, line in enumerate(f.readlines()):
             row = []
             for cols, h in enumerate(line.strip()):
-                row.append(int(h) if h != "." else -1)
-                if h != "." and int(h) == 0:
+                row.append(int(h))
+                if int(h) == 0:
                     starts.append(((rows, cols), (rows, cols)))
             map.append(row)
     rows += 1
@@ -33,5 +33,41 @@ def part1(file: str):
     return len(nines)
 
 
+def part2(file: str):
+    sum_of_ratings = 0
+    map = []
+    starts = []
+    with open(file, "r") as f:
+        for rows, line in enumerate(f.readlines()):
+            row = []
+            for cols, h in enumerate(line.strip()):
+                row.append(int(h))
+                if int(h) == 0:
+                    starts.append((rows, cols))
+            map.append(row)
+    rows += 1
+    cols += 1
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    while len(starts) > 0:
+        new_starts = []
+        for i, j in starts:
+            for di, dj in dirs:
+                k, l = i + di, j + dj
+                if (
+                    k >= 0
+                    and k < rows
+                    and l >= 0
+                    and l < cols
+                    and map[k][l] == map[i][j] + 1
+                ):
+                    if map[k][l] == 9:
+                        sum_of_ratings += 1
+                    else:
+                        new_starts.append((k, l))
+        starts = new_starts
+    return sum_of_ratings
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec10/test.py
+++ 2024/dec10/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec10(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 36)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 81)
 
 
 if __name__ == "__main__":
