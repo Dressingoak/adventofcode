9c00303d24f6b38c99f44299dadadc00f6fe9edb
Andreas Jessen <dressingoak@gmail.com>
2024-12-14 07:36:32 +0100

    Solution to Dec 14 2024, part 2

--- 2024/dec14/solution.py
+++ 2024/dec14/solution.py
@@ -8,7 +8,7 @@ def plot(robots, size: tuple[int, int]):
         print("")
 
 
-def solve(file: str, size: tuple[int, int]):
+def solve_part1(file: str, size: tuple[int, int]):
     rows, cols = size[1], size[0]
     quadrants = {i: 0 for i in range(4)}
     robots = {}
@@ -41,12 +41,53 @@ def solve(file: str, size: tuple[int, int]):
     return safety_factor
 
 
+def solve_part2(file: str, size: tuple[int, int]):
+    rows, cols = size[1], size[0]
+    quadrants = {i: 0 for i in range(4)}
+    robots = []
+    with open(file, "r") as f:
+        for line in f.readlines():
+            p, v = line.split(" ")
+            p = p.split("=")[1].split(",")
+            p = (int(p[1]), int(p[0]))
+            v = v.split("=")[1].split(",")
+            v = (int(v[1]), int(v[0]))
+            robots.append((p, v))
+    c = 0
+    while True:
+        c += 1
+        robot_count = {}
+        for i in range(len(robots)):
+            p, v = robots[i]
+            p = ((p[0] + v[0]) % rows, (p[1] + v[1]) % cols)
+            robots[i] = (p, v)
+            if p not in robot_count:
+                robot_count[p] = 1
+            else:
+                robot_count[p] += 1
+        line = 10
+        for i in range(size[1] - line):
+            for j in range(size[0] - line):
+                # When a suspiciously long line of robots appear, maybe that is it?
+                if all((i, j + k) in robot_count for k in range(line)):
+                    plot(robot_count, size)
+                    return c
+
+
 def part1(file: str):
     if file == "input.txt":
-        return solve(file, (101, 103))
+        return solve_part1(file, (101, 103))
     else:
-        return solve(file, (11, 7))
+        return solve_part1(file, (11, 7))
+
+
+def part2(file: str):
+    if file == "input.txt":
+        return solve_part2(file, (101, 103))
+    else:
+        return 0  # Input needs to support it
 
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
