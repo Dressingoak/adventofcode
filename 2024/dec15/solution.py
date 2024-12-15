a174afe5ec087e13db2d07e06e086dbf2a8a81c7
Andreas Jessen <dressingoak@gmail.com>
2024-12-15 08:05:22 +0100

    Solution to Dec 15 2024, part 2

--- 2024/dec15/solution.py
+++ 2024/dec15/solution.py
@@ -83,5 +83,110 @@ def part1(file: str):
     return gps
 
 
+def affected_boxes(map, pos, delta):
+    i, j = pos
+    di, dj = delta
+    k, l = i + di, j + dj
+    if di != 0:
+        match map[k][l]:
+            case "[":
+                yield (k, l)
+                yield from affected_boxes(map, (k, l), delta)
+                yield from affected_boxes(map, (k, l + 1), delta)
+            case "]":
+                yield (k, l - 1)
+                yield from affected_boxes(map, (k, l - 1), delta)
+                yield from affected_boxes(map, (k, l), delta)
+    else:
+        match map[k][l]:
+            case "[":
+                yield (k, l)
+                yield from affected_boxes(map, (k, l + 1), delta)
+            case "]":
+                yield (k, l - 1)
+                yield from affected_boxes(map, (k, l - 1), delta)
+
+
+def get_affected_boxes(map, pos, delta):
+    unique_boxes = set(affected_boxes(map, pos, delta))
+    match delta:
+        case (-1, 0) | (0, -1):
+            return sorted(unique_boxes)
+        case (1, 0):
+            return sorted(unique_boxes, key=lambda b: (-b[0], b[1]))
+        case (0, 1):
+            return sorted(unique_boxes, key=lambda b: (b[0], -b[1]))
+
+
+def part2(file: str):
+    map = []
+    movements = []
+    parse_map = True
+    i = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line == "":
+                parse_map = False
+                continue
+            if parse_map:
+                row = []
+                for j, v in enumerate(line):
+                    match v:
+                        case "#" | ".":
+                            row += [v, v]
+                        case "O":
+                            row += ["[", "]"]
+                        case "@":
+                            pos = (i, j * 2)
+                            row += [".", "."]
+                map.append(row)
+                i += 1
+            else:
+                movements += [_ for _ in line]
+    i, j = pos
+
+    # print("Initial state:")
+    # plot(map, (i, j))
+    # print()
+
+    for movement in movements:
+        # print(f"Move {movement}:")
+        match movement:
+            case "^":
+                di, dj = -1, 0
+            case "v":
+                di, dj = 1, 0
+            case ">":
+                di, dj = 0, 1
+            case "<":
+                di, dj = 0, -1
+        if map[i + di][j + dj] != "#":
+            moveable = True
+            boxes = get_affected_boxes(map, (i, j), (di, dj))
+            for k, l in boxes:
+                if map[k + di][l + dj] == "#" or map[k + di][l + dj + 1] == "#":
+                    moveable = False
+                    break
+            if moveable:
+                for k, l in boxes:
+                    for n in range(1, -1, -1) if dj > 0 else range(2):
+                        map[k][l + n], map[k + di][l + dj + n] = (
+                            map[k + di][l + dj + n],
+                            map[k][l + n],
+                        )
+                i += di
+                j += dj
+        # plot(map, (i, j))
+        # print()
+    gps = 0
+    for i, row in enumerate(map):
+        for j, v in enumerate(row):
+            if v == "[":
+                gps += 100 * i + j
+    return gps
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2024/dec15/test.py
+++ 2024/dec15/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec15(unittest.TestCase):
@@ -8,6 +8,9 @@ class TestDec15(unittest.TestCase):
 
     def test_part1_small(self):
         self.assertEqual(part1("test_small.txt"), 2028)
+
+    def test_part2(self):
+        self.assertEqual(part2("test.txt"), 9021)
 
 
 if __name__ == "__main__":
