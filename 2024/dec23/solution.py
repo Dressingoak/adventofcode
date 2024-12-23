a0b9a208cdcb4ee99741966fe471327f6bf0198d
Andreas Jessen <dressingoak@gmail.com>
2024-12-23 10:04:51 +0100

    Solution to Dec 23 2024, part 2

--- 2024/dec23/solution.py
+++ 2024/dec23/solution.py
@@ -28,11 +28,48 @@ def part1(file: str):
             else:
                 triplets[k] += 1
     for k, c in triplets.items():
-        if c >= 2:
+        if c == 3:
             if any(_.startswith("t") for _ in k):
                 sum += 1
     return sum
 
 
+def part2(file: str):
+    connections = {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            l, r = line.strip().split("-")
+            for x, y in [(l, r), (r, l)]:
+                if x not in connections:
+                    connections[x] = [y]
+                else:
+                    connections[x].append(y)
+
+    def connected(c1, c2):
+        return c2 in connections[c1]
+
+    smallest = 1
+    all_combs = []
+    for k, c in connections.items():
+        arr = [k, *c]
+        combs = []
+        for n in range(smallest, len(arr)):
+            for comb in combinations(arr, n):
+                m = len(comb)
+                cc = 0
+                for i in range(m):
+                    for j in range(i):
+                        cc += connected(comb[i], comb[j])
+                if cc == (m - 1) * m // 2:
+                    combs.append(comb)
+        if len(combs) > 0:
+            s = max(combs, key=lambda x: len(x))
+            smallest = len(s)
+            all_combs.append(s)
+    largest = max(all_combs, key=lambda x: len(x))
+    return ",".join(sorted(largest))
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
