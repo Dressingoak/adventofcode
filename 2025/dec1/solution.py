290de7b9421166f8260027e96c50d0c5fe9dbdf5
Andreas Jessen <dressingoak@gmail.com>
2025-12-01 09:13:38 +0100

    Solution to Dec 1 2025, part 2

--- 2025/dec1/solution.py
+++ 2025/dec1/solution.py
@@ -10,5 +10,26 @@ def part1(file: str):
     return sum
 
 
+def part2(file: str):
+    dial = 50
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            r = int(line[1:].strip())
+            m = r // 100
+            r %= 100
+            match line[0]:
+                case "L":
+                    if dial == 0:
+                        dial = 100
+                    sum += int(r >= dial) + m
+                    dial = (dial - r) % 100
+                case "R":
+                    sum += int((r + dial) >= 100) + m
+                    dial = (dial + r) % 100
+    return sum
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
