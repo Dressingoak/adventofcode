9f8ac36c008a9fdde4f455cf7b7afbfc8d4cd400
Andreas Jessen <dressingoak@gmail.com>
2025-12-02 10:11:16 +0100

    Update solution.py

--- 2025/dec2/solution.py
+++ 2025/dec2/solution.py
@@ -7,13 +7,23 @@ def digit_length(a) -> int:
     return m
 
 
-def is_invalid(a) -> bool:
+def is_invalid_exactly_twice(a) -> bool:
     if (m := digit_length(a)) % 2 == 1:
         return False
     else:
         n = m // 2
         s = str(a)
         return s[0:n] == s[n:m]
+
+
+def is_invalid_at_least_twice(a) -> bool:
+    s = str(a)
+    m = len(s)
+    for n in range(1, m // 2 + 1):
+        if m % n == 0:
+            if len(set(s[i : i + n] for i in range(0, m, n))) == 1:
+                return True
+    return False
 
 
 def part1(file: str):
@@ -23,10 +33,23 @@ def part1(file: str):
             l, h = r.split("-")
             l, h = int(l), int(h)
             for n in range(l, h + 1):
-                if is_invalid(n):
+                if is_invalid_exactly_twice(n):
+                    sum += n
+    return sum
+
+
+def part2(file: str):
+    sum = 0
+    with open(file, "r") as f:
+        for r in f.readline().strip().split(","):
+            l, h = r.split("-")
+            l, h = int(l), int(h)
+            for n in range(l, h + 1):
+                if is_invalid_at_least_twice(n):
                     sum += n
     return sum
 
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
