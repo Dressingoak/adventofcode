1002e75d87a40a513d09a252ad8c4f47f723d6bd
Andreas Jessen <dressingoak@gmail.com>
2025-12-03 10:16:51 +0100

    Solution to Dec 3 2025, part 2 (non-optimized)

--- 2025/dec3/solution.py
+++ 2025/dec3/solution.py
@@ -17,5 +17,34 @@ def part1(file: str):
     return sum
 
 
+def maximizer(a, lst, m):
+    if m == 0:
+        return a
+    if (n := len(lst)) < m:
+        return None
+    d = max(lst[0 : (n - m + 1)])
+    x = []
+    for i in range(n - m + 1):
+        if lst[i] == d:
+            x.append(
+                maximizer(
+                    a * 10 + d if a is not None else d,
+                    lst[i + 1 :],
+                    m - 1,
+                )
+            )
+    return max(x)
+
+
+def part2(file: str):
+    sum = 0
+    with open(file, "r") as f:
+        for line in f.readlines():
+            joltages = [int(_) for _ in line.strip()]
+            sum += maximizer(None, joltages, 12)
+    return sum
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2025/dec3/test.py
+++ 2025/dec3/test.py
@@ -1,10 +1,15 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec3(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 357)
+
+
+class TestDec3(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(part2("test.txt"), 3121910778619)
 
 
 if __name__ == "__main__":
