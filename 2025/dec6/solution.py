25d2691ec9215f1e13fa05217c2cdae6b37aae76
Andreas Jessen <dressingoak@gmail.com>
2025-12-06 11:54:31 +0100

    Solution to Dec 6 2025, part 2

--- 2025/dec6/solution.py
+++ 2025/dec6/solution.py
@@ -21,6 +21,37 @@ def part1(file: str):
     return s
 
 
+def part2(file: str):
+    chars = []
+    with open(file, "r") as f:
+        for line in f.readlines():
+            chars.append(line)
+    n, m = len(chars), len(chars[0])
+    chars_t = []
+    for j in range(m):
+        chars_t.append([chars[i][j] for i in range(n)])
+    s = 0
+    operator = None
+    ss = None
+    for line in chars_t:
+        if operator is None:
+            operator = line[-1]
+            if operator == "+":
+                ss = 0
+            else:
+                ss = 1
+        if (r := "".join(line)[:-1].strip()) == "":
+            s += ss
+            operator = None
+            ss = None
+            continue
+        if operator == "+":
+            ss += int(r)
+        else:
+            ss *= int(r)
+    return s
+
 
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
