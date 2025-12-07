f4d2c2b270189792d809bb2a0fe678e2ad0baa55
Andreas Jessen <dressingoak@gmail.com>
2025-12-07 08:57:56 +0100

    Solution to Dec 7 2025, part 2

--- 2025/dec7/solution.py
+++ 2025/dec7/solution.py
@@ -19,5 +19,41 @@ def part1(file: str):
     return splits
 
 
+timelines = {}
+
+
+def paths(i: int, j: int, beams: dict[int, set[int]]):
+    if (p := timelines.get((i, j))) is not None:
+        return p
+    if i in beams and j in beams[i]:
+        timelines[(i, j)] = (
+            p := (paths(i + 1, j - 1, beams) + paths(i + 1, j + 1, beams))
+        )
+        return p
+    elif i > max(beams.keys()):
+        timelines[(i, j)] = 1
+        return 1
+    else:
+        timelines[(i, j)] = (p := paths(i + 1, j, beams))
+        return p
+
+
+def part2(file: str):
+    beams = {}
+    with open(file, "r") as f:
+        for i, line in enumerate(f.readlines()):
+            for j, char in enumerate(line.strip()):
+                match char:
+                    case "S":
+                        start = (i, j)
+                    case "^":
+                        if i in beams:
+                            beams[i].add(j)
+                        else:
+                            beams[i] = set((j,))
+    return paths(*start, beams)
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
