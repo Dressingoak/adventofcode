1e44a1b95711deb85a98666e31075a6ab1d04f3e
Andreas Jessen <dressingoak@gmail.com>
2025-12-08 11:41:26 +0100

    Solution to Dec 8 2025, part 2

--- 2025/dec8/solution.py
+++ 2025/dec8/solution.py
@@ -38,5 +38,43 @@ def part1(file: str, connections: int = 1000):
     return sizes[0] * sizes[1] * sizes[2]
 
 
+def part2(file: str):
+    junction_boxes: list[tuple[int, int, int]] = []
+    junction_box_distances: list[tuple[int, tuple[int, int]]] = []
+    circuts: list[int, set[int]] = []
+    with open(file, "r") as f:
+        for line in f.readlines():
+            x, y, z = line.strip().split(",")
+            junction_boxes.append((int(x), int(y), int(z)))
+    n = len(junction_boxes)
+
+    for i in range(n):
+        x1, y1, z1 = junction_boxes[i]
+        for j in range(i + 1, n):
+            x2, y2, z2 = junction_boxes[j]
+            d2 = (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+            junction_box_distances.append((d2, (i, j)))
+    junction_box_distances.sort()
+
+    for d2, (i, j) in junction_box_distances:
+        connected = False
+        for l in range(len(circuts)):
+            if i in circuts[l] or j in circuts[l]:
+                circuts[l].add(i)
+                circuts[l].add(j)
+                connected = True
+        if not connected:
+            circuts.append(set([i, j]))
+        for k in range(m := len(circuts)):
+            for l in range(k + 1, m):
+                if len(circuts[k].intersection(circuts[l])) > 0:
+                    circuts[k].update(circuts[l])
+                    circuts[l] = set()
+        circuts = [circut for circut in circuts if len(circut) > 0]
+        if len(circuts) == 1 and len(circuts[0]) == n:
+            return junction_boxes[i][0] * junction_boxes[j][0]
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2025/dec8/test.py
+++ 2025/dec8/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec8(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt", connections=10), 40)
+
+    def test_part1(self):
+        self.assertEqual(part2("test.txt"), 25272)
 
 
 if __name__ == "__main__":
