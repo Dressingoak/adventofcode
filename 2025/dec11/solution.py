0fb8f468ce856ef8a00ea9bea1afad15d2a538bd
Andreas Jessen <dressingoak@gmail.com>
2025-12-11 09:16:37 +0100

    Solution to Dec 11 2025, part 2

--- 2025/dec11/solution.py
+++ 2025/dec11/solution.py
@@ -23,5 +23,33 @@ def part1(file: str):
     return ways("you")
 
 
+def part2(file: str):
+    adj = {}
+    with open(file, "r") as f:
+        for line in f.readlines():
+            src, *dst = line.strip().split(" ")
+            adj[src[:-1]] = dst
+
+    mem = {}
+
+    def ways(src, dst):
+        if (n := mem.get((src, dst))) is not None:
+            return n
+        if src == dst:
+            n = 1
+        elif src not in adj:
+            n = 0
+        else:
+            n = sum(ways(s, dst) for s in adj[src])
+        mem[(src, dst)] = n
+        return n
+
+    p1 = ways("svr", "dac") * ways("dac", "fft") * ways("fft", "out")
+    p2 = ways("svr", "fft") * ways("fft", "dac") * ways("dac", "out")
+
+    return p1 + p2
+
+
 if __name__ == "__main__":
     print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
--- 2025/dec11/test.py
+++ 2025/dec11/test.py
@@ -1,10 +1,13 @@
 import unittest
-from solution import part1
+from solution import part1, part2
 
 
 class TestDec11(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("test.txt"), 5)
+
+    def test_part2(self):
+        self.assertEqual(part2("test2.txt"), 2)
 
 
 if __name__ == "__main__":
